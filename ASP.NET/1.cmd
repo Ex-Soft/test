80aca2a11d1f4e359cff235ca3f8c8612b175087
Ex_Soft <4others2@gmail.com>
2016-11-15 16:35:53 +0200

    ASP.NET (*.cmd)

--- ASP.NET/1.cmd
+++ ASP.NET/1.cmd
@@ -3,13 +3,13 @@
 if -%1-==-- goto help
 if -%2-==-- goto help
 
-d:\arc\7za.exe a -r -mx9 -x@asp.lst "%1" "%2"
+7za.exe a -r -mx9 -xr@asp.lst "%1" "%2"
 
 goto end
 
 :help
 echo.
-echo Usage: 1.bat ^<archive_name^> ^<directory^>
+echo Usage: 1 ^<archive_name^> ^<directory^>
 echo.
 pause > nul
 goto end
