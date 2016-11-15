6a42168bf9e4b5671500eba563942fbbff2502ab
Ex_Soft <4others2@gmail.com>
2016-11-15 04:48:51 +0200

    vc (*.cmd)

--- vc/diff.cmd
+++ vc/diff.cmd
@@ -1,5 +1,6 @@
 @echo off
 set lng=vc
-set src=test_vc.7z
+set ext=7z
+set src=test_%lng%.%ext%
 set dest="%~dp0test"
-call ..\diff.cmd %lng% %src% %dest% -ini:cc_vc.ini
+call ..\diff.cmd %lng% %src% %dest% -ini:cc_%lng%.ini %1
