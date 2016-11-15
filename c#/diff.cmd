96bd9b49774f3d8d027a1d7fe8d208d8d76b6235
Ex_Soft <4others2@gmail.com>
2016-11-15 06:35:36 +0200

    c# (*.cmd)

--- c#/diff.cmd
+++ c#/diff.cmd
@@ -1,5 +1,6 @@
 @echo off
 set lng=c#
-set src=test_cs.7z
+set ext=7z
+set src=test_%lng%.%ext%
 set dest="%~dp0test"
-call ..\diff.cmd %lng% %src% %dest% -ini:cc_c#.ini
+call ..\diff.cmd %lng% %src% %dest% -ini:cc_%lng%.ini %1
