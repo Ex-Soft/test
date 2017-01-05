cd71d13b9db1aa97ae04dbfcdadc9e5fa7a7a3ab
Ex_Soft <4others2@gmail.com>
2017-01-05 07:40:36 +0200

    merge

--- c#/test/generics/TestList/main.cs
+++ c#/test/generics/TestList/main.cs
@@ -33,49 +33,64 @@ namespace TestList
 		}
 	}
 
+    class B : A
+    {}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			List<int>
-				LI = new List<int>();
+		    var listOfB = new List<B>
+		    {
+		        new B()
+		    };
+
+		    var listOfAI = new List<A>();
+
+            listOfB.ForEach((item) => { listOfAI.Add(item); });
+
+            List<int>
+				listOfIntI = new List<int>();
 
             int
-                tmpInt;
+                tmpInt,
+                defaultInt = default(int);
 
-		    tmpInt = LI.FindIndex(element => element == 13);
+		    tmpInt = listOfIntI.FindIndex(element => element == 13);
 
-            LI = new List<int>(new int[] { 5, 2, 9, 1 });
+            listOfIntI = new List<int>(new int[] { 5, 2, 9, 1 });
 
-            LI.ForEach(i => {
+            listOfIntI.ForEach(i => {
                 Console.WriteLine(i);
                 if (i < 5)
                     return;
                 Console.WriteLine(i);
             });
 
-			Console.WriteLine(LI.ToString());
+			Console.WriteLine(listOfIntI.ToString());
 
 			// Finds first element greater than 5
-			tmpInt = LI.Find(item => item > 5);
-			tmpInt = LI.Find(item => item > 20);
+			tmpInt = listOfIntI.Find(item => item > 5);
+			tmpInt = listOfIntI.Find(item => item > 20);
 
 			bool
 				exists;
 
-			exists = LI.Exists(item => item > 5);
-			exists = LI.Exists(item => item > 20);
+			exists = listOfIntI.Exists(item => item > 5);
+			exists = listOfIntI.Exists(item => item > 20);
 
-			tmpInt = LI.IndexOf(5);
-			tmpInt = LI.IndexOf(20);
+			tmpInt = listOfIntI.IndexOf(5);
+			tmpInt = listOfIntI.IndexOf(20);
 
-			tmpInt = LI.Find(delegate(int val) { return val == 5; });
-			tmpInt = LI.Find(delegate(int val) { return val == 20; });
+			tmpInt = listOfIntI.Find(delegate(int val) { return val == 5; });
+            System.Diagnostics.Debug.WriteLine(tmpInt != defaultInt ? "oB!" : "Tampax");
+			tmpInt = listOfIntI.Find(delegate(int val) { return val == 20; });
+            System.Diagnostics.Debug.WriteLine(tmpInt != defaultInt ? "oB!" : "Tampax");
 
-			List<string>
-				LS = new List<string>(new string[] { "2nd", "3rd", "1st", "4th" });
+            List<string>
+				listOfStringI = new List<string>(new string[] { "2nd", "3rd", "1st", "4th" });
 
-			LS.ForEach((item) =>
+			listOfStringI.ForEach((item) =>
 			{
 				if (item == "3rd")
 					return;
@@ -86,62 +101,77 @@ namespace TestList
 			string
 				tmpString;
 
-			tmpString = LS.Find(item => item == "3rd");
-			tmpString = LS.Find(item => item == "5th");
+			tmpString = listOfStringI.Find(item => item == "3rd");
+			tmpString = listOfStringI.Find(item => item == "5th");
 
-			exists = LS.Exists(item => item == "3rd");
-			exists = LS.Exists(item => item == "5th");
+			exists = listOfStringI.Exists(item => item == "3rd");
+			exists = listOfStringI.Exists(item => item == "5th");
 
-			tmpInt = LS.IndexOf("3rd");
-			tmpInt = LS.IndexOf("5th");
+			tmpInt = listOfStringI.IndexOf("3rd");
+			tmpInt = listOfStringI.IndexOf("5th");
 
-			tmpString = LS.Find(delegate(string val) { return val == "3rd"; });
-			tmpString = LS.Find(delegate(string val) { return val == "5th"; });
+			tmpString = listOfStringI.Find(delegate(string val) { return val == "3rd"; });
+			tmpString = listOfStringI.Find(delegate(string val) { return val == "5th"; });
 
-			A
-				a = new A("1st");
+		    listOfAI = new List<A>
+		    {
+		        new A("1st"),
+                new A("2nd"),
+                new A("3rd"),
+                new A("4th"),
+                new A("5th")
+		    };
 
-			List<A>
-				L1 = new List<A>();
+		    A
+                a,
+		        defaultA = default(A);
 
-			L1.Add(a);
+		    a = listOfAI.Find(delegate(A _a_) { return _a_.FString == "3rd"; });
+            System.Diagnostics.Debug.WriteLine(a != defaultA ? "oB!" : "Tampax");
+            a = listOfAI.Find(delegate (A _a_) { return _a_.FString == "blah-blah-blah"; });
+            System.Diagnostics.Debug.WriteLine(a != defaultA ? "oB!" : "Tampax");
+
+            a = new A("1st");
+            listOfAI = new List<A>();
+
+            listOfAI.Add(a);
 			a.FString = "2nd";
-			L1.Add(a);
+            listOfAI.Add(a);
 
 			A
 				b = new A("3rd");
 
-			L1.Add(new A(b));
+            listOfAI.Add(new A(b));
 			b.FString = "4th";
-			L1.Add(new A(b));
+            listOfAI.Add(new A(b));
 
-			Console.WriteLine("L1:");
-			L1.ForEach((item) => { Console.WriteLine(item.FString); });
+			Console.WriteLine("listOfAI:");
+            listOfAI.ForEach((item) => { Console.WriteLine(item.FString); });
 			Console.WriteLine();
 
 			List<A>
-				L2 = new List<A>(L1);
+				listOfAII = new List<A>(listOfAI);
 
-			Console.WriteLine("L2:");
-			L1.ForEach((item) => { Console.WriteLine(item.FString); });
+			Console.WriteLine("listOfAII:");
+            listOfAI.ForEach((item) => { Console.WriteLine(item.FString); });
 			Console.WriteLine();
 
-			L1.ForEach((item) => { item.FString=";-)"; });
+            listOfAI.ForEach((item) => { item.FString=";-)"; });
 
-			Console.WriteLine("L1:");
-			L1.ForEach((item) => { Console.WriteLine(item.FString); });
+			Console.WriteLine("listOfAI:");
+            listOfAI.ForEach((item) => { Console.WriteLine(item.FString); });
 			Console.WriteLine();
 			Console.WriteLine("a={0}",a.FString);
-			Console.WriteLine("L2:");
-			L2.ForEach((item) => { Console.WriteLine(item.FString); });
+			Console.WriteLine("listOfAII:");
+			listOfAII.ForEach((item) => { Console.WriteLine(item.FString); });
 			Console.WriteLine();
 
             List<int>
-                LI2 = new List<int>(new int[] { 5, 2, 9, 1 });
+                listOfIntII = new List<int>(new int[] { 5, 2, 9, 1 });
 
-            LI = new List<int>(new int[] { 5, 2, 9, 1 });
-            LI.AddRange(LI2);
-            LI.ForEach((item) => Console.WriteLine(item));
+            listOfIntI = new List<int>(new int[] { 5, 2, 9, 1 });
+            listOfIntI.AddRange(listOfIntII);
+            listOfIntI.ForEach((item) => Console.WriteLine(item));
 		}
 	}
 }
