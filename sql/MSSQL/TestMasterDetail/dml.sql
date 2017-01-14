b93fccd34a4fc75e35435b18ed445d3b023a4cd9
Ex_Soft <4others2@gmail.com>
2017-01-14 22:10:51 +0200

    sql

--- sql/MSSQL/TestMasterDetail/dml.sql
+++ sql/MSSQL/TestMasterDetail/dml.sql
@@ -3,7 +3,43 @@
 from
 	dbo.TestMaster [master]
 	left join dbo.TestDetail [detail] on [detail].IdMaster = [master].Id
-order by [master].Id
+order by [master].Id;
 
--- update dbo.TestDetail set Val = N'' where id = 16
--- update dbo.TestDetail set Val = null where id = 16
+-- update dbo.TestDetail set Val = N'' where id = 16;
+-- update dbo.TestDetail set Val = null where id = 16;
+
+;with cte
+as
+(
+	select
+		[master].Id,
+		[master].Val as MasterVal,
+		[detail].Val as DetailVal,
+		row_number() over (partition by [master].Id order by [detail].Id) as rn
+	from
+		TestMaster [master]
+		join TestDetail [detail] on [detail].IdMaster = [master].Id
+)
+select
+	*
+from
+	cte
+where
+	rn = 1;
+
+select
+	[master].Id,
+	[master].Val as MasterVal,
+	[detail].Val as DetailVal
+ from
+	TestMaster [master]
+	cross apply
+	(
+		select top 1
+			Val
+		from
+			TestDetail [detail]
+		where
+			IdMaster = [master].Id
+		order by Id
+	) as [detail];
