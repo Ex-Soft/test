c90ef80a952b2e2d89a6790cee05a70e20721450
Ex_Soft <4others2@gmail.com>
2017-01-17 03:00:24 +0200

    UnitTest (by Community)

---
+++ c#/test/TestUnitTest/TestUnitTestCommunity/ClassLibrary.Test/TestMath.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassLibrary.Test
+{
+    [TestClass]
+    public class TestMath
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            // arrange
+            var math = new Math();
+
+            // act
+            var result = math.Add(1, 2);
+
+            // assert
+            Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void TestSub()
+        {
+            // arrange
+            var math = new Math();
+
+            // act
+            var result = math.Sub(1, 2);
+
+            // assert
+            Assert.AreEqual(result, -1);
+        }
+    }
+}
---
+++ c#/test/TestUnitTest/TestUnitTestCommunity/ClassLibrary/Math.cs
@@ -0,0 +1,25 @@
+﻿namespace ClassLibrary
+{
+    public class Math
+    {
+        public int Add(int leftOp, int rightOp)
+        {
+            return leftOp + rightOp;
+        }
+
+        public int Sub(int leftOp, int rightOp)
+        {
+            return leftOp - rightOp;
+        }
+
+        public int Mul(int leftOp, int rightOp)
+        {
+            return leftOp * rightOp;
+        }
+
+        public int Div(int leftOp, int rightOp)
+        {
+            return leftOp / rightOp;
+        }
+    }
+}
