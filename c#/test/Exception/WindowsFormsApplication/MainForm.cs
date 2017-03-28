193d676c2773fb0834e4332584beb24d6c91f7ce
Ex_Soft <4others2@gmail.com>
2017-03-28 09:17:11 +0300

    c#

---
+++ c#/test/Exception/WindowsFormsApplication/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonException_Click(object sender, EventArgs e)
+        {
+            throw new Exception("MainForm.Exception");
+        }
+
+        private void buttonAggregateException_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Task.Factory.StartNew(Run);
+            }
+            catch (AggregateException eException)
+            {
+                Debug.WriteLine("\nThe following exceptions have been thrown by WaitAll(): (THIS WAS EXPECTED)");
+                for (int j = 0; j < eException.InnerExceptions.Count; j++)
+                {
+                    Debug.WriteLine("\n-------------------------------------------------\n{0}", eException.InnerExceptions[j].ToString());
+                }
+            }
+        }
+
+        private void Run()
+        {
+            throw new Exception("MainForm.Run.Exception");
+        }
+    }
+}
