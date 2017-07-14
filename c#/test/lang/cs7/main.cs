57f908840354dddffb5fc19b8e8c0ec7b88a938f
Ex_Soft <4others2@gmail.com>
2017-07-14 22:00:48 +0300

    c#

---
+++ c#/test/lang/cs7/main.cs
@@ -0,0 +1,22 @@
+﻿// https://blogs.msdn.microsoft.com/dotnet/2017/03/09/new-features-in-c-7-0/
+// https://www.erikheemskerk.nl/csharp-7-0-improvements/
+
+using static System.Console;
+
+namespace cs7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodWithOutParameters(out int x, out int y);
+            WriteLine($"({x}, {y})");
+        }
+
+        static void MethodWithOutParameters(out int x, out int y)
+        {
+            x = 1;
+            y = 2;
+        }
+    }
+}
