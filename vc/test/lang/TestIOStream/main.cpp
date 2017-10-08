0fcf5e875b39b47dffb039cab8a7d397d4758b75
Ex_Soft <4others2@gmail.com>
2017-10-08 21:30:47 +0300

    VC

--- vc/test/lang/TestIOStream/main.cpp
+++ vc/test/lang/TestIOStream/main.cpp
@@ -1,13 +1,54 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     int
         a = 10,
         b,
-        c;
+        c,
+		result;
 
+	std::string
+		str;
+
+	std::ios_base::iostate
+		state;
+
+	std::streampos
+		pos;
+
+	if (!(std::cin >> b))
+	{
+		std::cout << "wrong value" << std::endl;
+
+		state = std::cin.rdstate();
+		std::cout << "cin.rdstate() " << state << std::endl;
+
+		if (state & std::ios_base::failbit)
+			std::cout << "ios_base::failbit" << std::endl;
+		if (state & std::ios_base::badbit)
+			std::cout << "ios_base::badbit" << std::endl;
+		if (state & std::ios_base::goodbit)
+			std::cout << "ios_base::goodbit" << std::endl;
+		if (state & std::ios_base::eofbit)
+			std::cout << "ios_base::eofbit" << std::endl;
+
+		std::cout << "cin.fail() " << std::cin.fail() << std::endl;
+		std::cout << "cin.bad() " << std::cin.bad() << std::endl;
+		std::cout << "cin.eof() " << std::cin.eof() << std::endl;
+		std::cout << "cin.good() " << std::cin.good() << std::endl;
+
+		std::cout << "cin.gcount() " << std::cin.gcount() << std::endl;
+
+		pos = std::cin.tellg();
+		result = std::cin.peek();
+		//result = std::cin.sync();
+		std::cin.clear();
+		std::cin >> str;
+	}
+	
     std::cout << "a=" << std::setw(10) << std::setfill('0') << std::hex << std::setiosflags(std::ios::showbase) << a << std::endl;
 
     std::cout << "> ";
