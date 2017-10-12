e6f0a6568afd33e58606adc01d19f151cf0dca08
Ex_Soft <4others2@gmail.com>
2017-10-12 20:39:53 +0300

    c#

---
+++ c#/test/TestUnitTest/TestFakes/ClassLibrary2/ClassWithPrivateMembers.cs
@@ -0,0 +1,22 @@
+﻿namespace ClassLibrary2
+{
+    public class ClassWithPrivateMembers
+    {
+        private int _privateFInt;
+        private int PrivatePInt { get; set; }
+
+        public ClassWithPrivateMembers(int fInt = 0, int pInt = 0)
+        {
+            _privateFInt = fInt;
+            PrivatePInt = pInt;
+        }
+
+        private int Mul(int param)
+        {
+            _privateFInt *= param;
+            PrivatePInt *= param;
+
+            return _privateFInt * PrivatePInt;
+        }
+    }
+}
