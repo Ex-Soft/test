d834acfbd2e488d33b80d99681be087a1d5c2581
Ex_Soft <4others2@gmail.com>
2017-10-12 10:44:57 +0300

    c#

---
+++ c#/test/TestUnitTest/TestFakes/ClassLibrary2/ClassA.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassLibrary2
+{
+    public class ClassA
+    {
+        public string FString { get; set; }
+
+        public ClassA(string fString = "")
+        {
+            FString = fString;
+        }
+
+        public ClassA(ClassA obj) : this(obj.FString)
+        {}
+    }
+}
