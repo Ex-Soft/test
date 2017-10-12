936f27ee47e1939744da9ac8b409329f8a9f9977
Ex_Soft <4others2@gmail.com>
2017-10-12 10:43:51 +0300

    sql

--- sql/MSSQL/testdbXPObjectType.sql
+++ sql/MSSQL/testdbXPObjectType.sql
@@ -9,3 +9,5 @@ order by AssemblyName, TypeName
 --insert into XPObjectType (TypeName, AssemblyName) values (N'TestOverridedGrid.Db.TestTable4Types', N'TestOverridedGrid')
 --insert into XPObjectType (TypeName, AssemblyName) values (N'TestOverridedGrid.Db.TestMaster', N'TestOverridedGrid')
 --insert into XPObjectType (TypeName, AssemblyName) values (N'TestOverridedGrid.Db.TestDetail', N'TestOverridedGrid')
+
+--delete from XPObjectType
