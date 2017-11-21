cf6994b7c38b8866e7f8b45430849842e0e047e8
Ex_Soft <4others2@gmail.com>
2017-11-21 09:47:44 +0200

    c#

---
+++ c#/test/TestUnitTest/TestFakes/UnitTestProject/EntityATests.cs
@@ -0,0 +1,46 @@
+﻿using ClassLibrary2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class EntityATests
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            Debug.WriteLine("ClassInitialize()");
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Debug.WriteLine("TestInitialize()");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Debug.WriteLine("TestCleanup()");
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Debug.WriteLine("ClassCleanup()");
+        }
+
+        [TestMethod]
+        public void Foo1Test()
+        {
+            Assert.AreEqual("123", EntityA.Foo1("123"));
+        }
+
+        [TestMethod]
+        public void Foo2Test()
+        {
+            Assert.AreEqual("123", EntityA.Foo2("123"));
+        }
+    }
+}
---
+++ c#/test/TestUnitTest/TestFakes/UnitTestProject/InitUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class InitUnitTests
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            Debug.WriteLine("AssemblyInitialize()");
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            Debug.WriteLine("AssemblyCleanup()");
+        }
+    }
+}
