a51f57f2e94da5310c7b3aff2372a9e0d427c20b
Ex_Soft <4others2@gmail.com>
2017-11-21 09:46:42 +0200

    sql

--- sql/MSSQL/TestMasterDetail/TestDetail.sql
+++ sql/MSSQL/TestMasterDetail/TestDetail.sql
@@ -10,6 +10,11 @@ create table TestDetail
 	Id int identity constraint "pkTestDetail" primary key,
 	IdMaster int constraint "TestDetailIdMasterNotNull" not null,
 	Val nvarchar(255),
+	FBit bit null,
 	constraint "fkTestMasterTestDetail" foreign key (IdMaster) references TestMaster(Id)
 )
+go
+
+if col_length(N'dbo.TestDetail', N'FBit') is null
+	alter table TestDetail add FBit bit null
 go
--- sql/MSSQL/TestMasterDetail/TestMaster.sql
+++ sql/MSSQL/TestMasterDetail/TestMaster.sql
@@ -8,6 +8,16 @@ go
 create table TestMaster
 (
 	Id int identity constraint "pkTestMaster" primary key,
-	Val nvarchar(255)
+	Val nvarchar(255),
+	FBit bit null,
+	IdForView as (Id)
 )
+go
+
+if col_length(N'dbo.TestMaster', N'FBit') is null
+	alter table TestMaster add FBit bit null
+go
+
+if col_length(N'TestMaster', N'IdForView') is null
+	alter table TestMaster add IdForView as (Id)
 go
