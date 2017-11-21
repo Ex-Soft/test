ba408e403845621efd959081f37cd33f19be9f5a
Ex_Soft <4others2@gmail.com>
2017-11-21 12:11:08 +0200

    sql

--- sql/MSSQL/TestMasterDetail/dml.sql
+++ sql/MSSQL/TestMasterDetail/dml.sql
@@ -3,8 +3,9 @@
 from
 	dbo.TestMaster [master]
 	left join dbo.TestDetail [detail] on [detail].IdMaster = [master].Id
-order by [master].Id;
+order by [master].Id, [detail].Val;
 
+-- update dbo.TestDetail set IdMaster = 1 where id = 2;
 -- update dbo.TestDetail set Val = N'' where id = 16;
 -- update dbo.TestDetail set Val = null where id = 16;
 
