e4d3f172474c3657d90a9eabeb8810867904e475
Ex_Soft <4others2@gmail.com>
2017-12-31 18:52:05 +0200

    cpp && asm

--- vc/test/lang/TestCall/main.cpp
+++ vc/test/lang/TestCall/main.cpp
@@ -1,32 +1,43 @@
-int fooC(int, int);
-void fooAsm(int);
+int fooC(int, int, int *, int &);
+void fooAsm(int, int, int *, int &);
 
 int main(int argc, char **argv)
 {
-	int result;
+	int
+		param1 = 0x6789,
+		param2 = 0xfefe,
+		result;
 
-	fooAsm(0xabcd);
+	fooAsm(0xabcd, 0x1234, &param1, param2);
 	__asm
 	{
 		mov	result, eax
 	}
 	
-	result = fooC(1, 2);
+	result = fooC(1, 2, &param1, param2);
 	
 	return 0;
 }
 
-int fooC(int a, int b)
+int fooC(int a, int b, int *c, int &d)
 {
-	return a + b;
+	return a + b + *c + d;
 }
 
-void fooAsm(int a)
+void fooAsm(int a, int b, int *c, int &d)
 {
 	__asm
 	{
-		mov	eax, [ebp + 08h]
+		mov	eax, dword ptr [ebp + 08h]
 		and	eax, 010101b
+		mov	ebx, dword ptr [ebp + 0ch]
+		add	eax, ebx
+		mov	ebx, dword ptr[ebp + 010h]
+		add eax, dword ptr [ebx]
+		mov	dword ptr [ebx], 0abcdh
+		mov	ebx, dword ptr[ebp + 014h]
+		add eax, dword ptr[ebx]
+		mov	dword ptr[ebx], 04321h
 	}
 }
 
