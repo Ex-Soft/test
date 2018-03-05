8ce89d6595b0c45219f25e53b92398a874a1c377
Ex_Soft <4others2@gmail.com>
2018-03-05 15:43:40 +0200

    c++

--- vc/test/lang/TestNew/main.cpp
+++ vc/test/lang/TestNew/main.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <new>
 #include <iostream>
 #include <iomanip>
--- vc/test/lang/TestPointer/main.cpp
+++ vc/test/lang/TestPointer/main.cpp
@@ -1,4 +1,4 @@
-//#include <iostream>
+#include <iostream>
 
 void swap_bad(int paramA, int paramB)
 {
@@ -33,7 +33,7 @@ int main(int argc, char** argv)
     swap_bad(a, b);
     swapByPtr(&a, &b);
     swapByReference(a, b);
-/*
+
     a = 10;
     b = 20;
 
@@ -60,26 +60,64 @@ int main(int argc, char** argv)
 
     ptrChar = str;
 
+	const int
+		SizeMaxX = 2,
+		SizeMaxY = 3;
+
     int
         array[5] = { 1, 2, 3, 4, 5},
-        darray[2][3] = { {10, 20, 30}, { 40, 50, 60 } };
+        darray[SizeMaxX][SizeMaxY] = { {10, 20, 30}, { 40, 50, 60 } };
 
     a = array[3];
     b = *(array + 3);
 
-    for(i=0; i<6; ++i)
+    for(i = 0; i < 6; ++i)
         std::cout << "array[" << i << "] = " << *(array + i) << std::endl;
 
     ptrInt = (int *)darray;
-    for(i=0; i<2; ++i)
-        for(j=0; j<3; ++j)
+    for(i = 0; i < SizeMaxX; ++i)
+        for(j = 0; j < SizeMaxY; ++j)
         {
             std::cout << "darray[" << i << "][" << j << "] = " << darray[i][j] << std::endl;
-            std::cout << "darray[" << i << "][" << j << "] = " << *(ptrInt + (3 * i) + j) << std::endl;
+            std::cout << "darray[" << i << "][" << j << "] = " << *(ptrInt + (SizeMaxY * i) + j) << std::endl;
         }
     
-    for(i=0; i<6; ++i)
+    for(i = 0; i < SizeMaxX * SizeMaxY; ++i)
         std::cout << "darray[" << i << "] = " << *(ptrInt + i) << std::endl;
-*/
+
+	ptrInt = new int[SizeMaxX * SizeMaxY];
+	memset(ptrInt, 0, SizeMaxX * SizeMaxY);
+
+	for (i = 0; i < SizeMaxX; ++i)
+		for (j = 0; j < SizeMaxY; ++j)
+		{
+			//std::cout << "darray[" << i << "][" << j << "] = " << ptrInt[i][j] << std::endl; // Error C2109 subscript requires array or pointer type
+			std::cout << "darray[" << i << "][" << j << "] = " << *(ptrInt + (SizeMaxY * i) + j) << std::endl;
+		}
+
+	delete []ptrInt;
+
+	int
+		**arr = 0;
+
+	arr = new int*[SizeMaxX];
+	for (i = 0; i < SizeMaxX; ++i)
+		arr[i] = new int[SizeMaxY];
+
+	for (i = 0; i < SizeMaxX; ++i)
+		for (j = 0; j < SizeMaxY; ++j)
+			arr[i][j] = i + j;
+
+	for (i = 0; i < SizeMaxX; ++i)
+		for (j = 0; j < SizeMaxY; ++j)
+		{
+			std::cout << "arr[" << i << "][" << j << "] = " << arr[i][j] << std::endl;
+		}
+
+	for (i = 0; i < SizeMaxX; ++i)
+		delete []arr[i];
+
+	delete []arr;
+
     return 0;
 }
