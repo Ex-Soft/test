882d83e5614bb98ea0b8631efcba35fd10865e36
Ex_Soft <4others2@gmail.com>
2018-03-15 23:33:55 +0200

    ASP.NET

--- ASP.NET/test/AnyTest2_1/LogII.cs
+++ ASP.NET/test/AnyTest2_1/LogII.cs
@@ -1,19 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Web;
+using System.IO;
 
 namespace LogII
 {
     public class LogII
     {
-        public static string MakeLogFileName(string fileName = "")
+        public static string MakeLogFileName(string logFileNameSuffix = "")
         {
-            return string.Empty;
+            DateTime
+                NowDateTime = DateTime.Now;
+
+            string
+                DirName = System.Web.HttpContext.Current.Server.MapPath(null) + "\\log",
+                LogFileName = NowDateTime.ToString("yy-MM-dd");
+
+            if ((bool)System.Web.HttpContext.Current.Application["HourLogFile" + logFileNameSuffix])
+                LogFileName += "_" + NowDateTime.Hour.ToString("D2") + "-00-00" + "_" + NowDateTime.AddHours((int)System.Web.HttpContext.Current.Application["DeltaHourLogFile" + logFileNameSuffix] - 1).Hour.ToString("D2") + "-59-59";
+
+            if ((bool)System.Web.HttpContext.Current.Application["SessionLogFile" + logFileNameSuffix])
+                LogFileName += "_" + System.Web.HttpContext.Current.Session.SessionID;
+
+            if (!Directory.Exists(DirName))
+                Directory.CreateDirectory(DirName);
+
+            LogFileName = DirName + "\\" + LogFileName + ".log";
+            System.Web.HttpContext.Current.Session["LogFileName" + logFileNameSuffix] = LogFileName;
+
+            return (LogFileName);
         }
 
-        public static void MakeFile(string logFileName1, string logFileName2, bool append)
+        public static void MakeFile(string FileName, string WriteString, bool Append)
         {
+            if (FileName == null
+                || WriteString == null
+                || (FileName = FileName.Trim()) == string.Empty
+                || WriteString.Trim() == string.Empty)
+                return;
 
+            StreamWriter
+                OutputFile = null;
+
+            lock (typeof(StreamWriter))
+            {
+                if ((OutputFile = new StreamWriter(FileName, Append, System.Text.Encoding.GetEncoding(1251))) != null && OutputFile.BaseStream != null && OutputFile.BaseStream.CanWrite)
+                {
+                    if (!OutputFile.AutoFlush)
+                        OutputFile.AutoFlush = true;
+
+                    if (Append && OutputFile.BaseStream.Position != 0)
+                        OutputFile.Write(Environment.NewLine);
+
+                    OutputFile.Write(DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.ffff") + '\t' + WriteString);
+                }
+
+                if (OutputFile != null)
+                    OutputFile.Close();
+            }
         }
     }
 }
