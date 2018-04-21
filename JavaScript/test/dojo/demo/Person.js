3ec80d52c1c91a54aab8b4dbc6ad4e9e61365a54
Ex_Soft <4others2@gmail.com>
2018-04-21 15:51:01 +0300

    dojo

--- JavaScript/test/dojo/demo/Person.js
+++ JavaScript/test/dojo/demo/Person.js
@@ -1,10 +1,16 @@
 define(["dojo/_base/declare"], function(declare){
-  return declare(null, {
-    constructor: function(name, age, residence){
-      this.name = name;
-      this.age = age;
-      this.residence = residence;
-    }
-  });
+	return declare(null, {
+		constructor: function(name, age, residence){
+			this.name = name;
+			this.age = age;
+			this.residence = residence;
+		},
+		getName: function(){
+			return this.name;
+		},
+		setName: function(name){
+			this.name = name;
+		}
+	});
 });
 
