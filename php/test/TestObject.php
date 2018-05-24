af2ab02ca8169ebc4fa22dc64b88896a384faef4
Ex_Soft <4others2@gmail.com>
2018-05-24 13:56:59 +0300

    php

--- php/test/TestObject.php
+++ php/test/TestObject.php
@@ -19,13 +19,43 @@ function echoObject($o) {
 }
 
 try {
-	echo phpinfo();
+	//echo phpinfo();
 
 	echo 'phpversion() ' . phpversion();
 	echo "<br/><br/>";
 
+	$a = (object)["p1" => 0, "p2" => "p2 (a)"];
+	$b = $a;
+
+	++$a->p1;
+	$a->p2 = "p2 (b)";
+	echo serialize($a);
+	echo "<br/>";
+	echo serialize($b);
+	echo "<br/>";
+	echo 'gettype($a) = ' . gettype($a) . "<br/>";
+	echo 'gettype($a) ' . (gettype($a) == "object" ? "=" : "!") . "= \"object\"<br/>";
+	echo 'gettype($a) ' . (gettype($a) === "object" ? "=" : "!") . "== \"object\"<br/>";
+	echo 'property_exists($a, "p1") = "' . property_exists($a, "p1") . "\"<br/>";
+	echo 'property_exists($a, "p10") = "' . property_exists($a, "p10") . "\"<br/>";
+	if (property_exists($a, "p1"))
+		echo "oB!<br/>";
+	if (!property_exists($a, "p10"))
+		echo "Tampax<br/>";
+	echo "<br/>";
+
 	$o = array("p1" => 1, "p2" => "p2", "p3" => array("1st", "2nd", "3rd"), "p4" => array("p1" => 1, "p2" => "p2"));
 	echo serialize($o);
+	echo "<br/>";
+	echo 'gettype($o) = ' . gettype($o) . "<br/>";
+	echo 'gettype($o) ' . (gettype($o) == "array" ? "=" : "!") . "= \"array\"<br/>";
+	echo 'gettype($o) ' . (gettype($o) === "array" ? "=" : "!") . "== \"array\"<br/>";
+	echo 'array_key_exists("p1", $o) = "' . array_key_exists("p1", $o) . "\"<br/>";
+	echo 'array_key_exists("p10", $o) = "' . array_key_exists("p10", $o) . "\"<br/>";
+	if (array_key_exists("p1", $o))
+		echo "oB!<br/>";
+	if (!array_key_exists("p10", $o))
+		echo "Tampax<br/>";
 	echo "<br/>";
 
 	print_r($o);
