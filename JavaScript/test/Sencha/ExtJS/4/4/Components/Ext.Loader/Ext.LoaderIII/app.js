42b9135a6a12966096ed5e2324a1bb6140cbba94
Ex_Soft <4others2@gmail.com>
2018-05-31 17:24:32 +0300

    ExtJS

---
+++ JavaScript/test/Sencha/ExtJS/4/4/Components/Ext.Loader/Ext.LoaderIII/app.js
@@ -0,0 +1,16 @@
+﻿Ext.Loader.setConfig({
+	enabled: true,
+	disableCaching: false
+});
+
+Ext.require([
+	"Ext.panel.Panel"
+]);
+
+Ext.onReady(function() {
+	var
+		p = Ext.create("Ext.panel.Panel", {
+			renderTo: Ext.getBody(),
+			html: "Ext.panel.Panel"
+		});
+});
---
+++ JavaScript/test/Sencha/ExtJS/4/4/Components/Ext.Loader/TestRequireVsUses/app.js
@@ -0,0 +1,32 @@
+﻿// http://www.sencha.com/forum/showthread.php?133191-What-s-the-difference-between-requires-and-uses-when-defining-a-class
+// http://www.sencha.com/forum/showthread.php?184725-Uses-vs-requires
+
+Ext.Loader.setConfig({
+	enabled: true,
+	disableCaching: false
+});
+
+Ext.require([
+	"Ext.view.*",
+	"Ext.grid.*"
+]);
+
+Ext.onReady(function() {
+	Ext.Loader.injectScriptElement("../../../../../../ExtJS/ExtJS4/locale/ext-lang-ru.js", onLoad, onError);
+});
+
+function onLoad() {
+	if(window.console && console.log)
+		console.log("onLoad(%o)", arguments);
+
+	var
+		now = new Date();
+
+	if(window.console && console.log)
+		console.log("%s, %i %s %i", Ext.Date.dayNames[now.getDay()], now.getDate(), Ext.Date.shortMonthNames[now.getMonth()], now.getFullYear());
+}
+
+function onError() {
+	if(window.console && console.log)
+		console.log("onError(%o)", arguments);
+}
