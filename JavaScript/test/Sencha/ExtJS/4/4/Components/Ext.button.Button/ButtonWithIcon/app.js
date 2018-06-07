6672eabdfa078cc3fe7d46446e4d40fe63cf9653
Ex_Soft <4others2@gmail.com>
2018-06-07 17:40:56 +0300

    ExtJS

--- JavaScript/test/Sencha/ExtJS/4/4/Components/Ext.button.Button/ButtonWithIcon/app.js
+++ JavaScript/test/Sencha/ExtJS/4/4/Components/Ext.button.Button/ButtonWithIcon/app.js
@@ -5,5 +5,39 @@
 			text: "Ext.button.Button",
 			iconCls: "iconTest",
 			iconAlign: "top"
+		}),
+		s = Ext.create("Ext.button.Split", {
+			renderTo: Ext.getBody(),
+			text: "Ext.button.Split",
+			handler: function(button, e) {
+				if (window.console && console.log)
+				{
+					console.log("Ext.button.Split.handler(%o)", arguments);
+					console.log("button.isXType(\"button\") = \"%s\"", button.isXType("button"));
+					console.log("button.isXType(\"splitbutton\") = \"%s\"", button.isXType("splitbutton"));
+
+					button.menu.items.items.forEach(function(item) {
+						console.log("\"%s\" xtype:\"%s\"", item.text, item.xtype);
+					});
+				}
+			},
+			menu: new Ext.menu.Menu({
+				items: [{
+					text: "Item# 1",
+					handler: function(button, e) {
+						if (window.console && console.log)
+							console.log("Ext.button.Split.menu.item.handler(%o) button.isXType(\"menuitem\") = \"%s\"", arguments, button.isXType("menuitem"));
+
+						button.setDisabled(true);
+					}
+				}, {
+					text: "Item# 2",
+					disabled: true,
+					handler: function(button, e) {
+						if (window.console && console.log)
+							console.log("Ext.button.Split.menu.item.handler(%o)", arguments);
+					}
+				}]
+			})
 		});
 });
