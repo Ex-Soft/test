be5ff801f45827170f15d6029802e02a31602dbc
Ex_Soft <4others2@gmail.com>
2018-06-19 20:57:21 +0300

    ExtJS

---
+++ JavaScript/test/Sencha/ExtJS/4/4/Components/Ext.panel.Panel/TestMixins/TestMixinSingleton.js
@@ -0,0 +1,16 @@
+﻿Ext.define("TestMixinSingleton", {
+	single: true,
+
+	constructor: function(config) {
+		if (window.lonsole && console.log)
+			console.log("TestMixinSingleton.ctor(%o)", arguments);
+
+		this.callParent([config]);
+
+		return this;
+	},
+
+	doSmth: function(){
+		this.html = "blah-blah-blah";
+	}
+});
