e3c78ed21bf92d86b4010a136842889326ff328e
Ex_Soft <4others2@gmail.com>
2018-06-27 18:04:16 +0300

    ExtJS

---
+++ JavaScript/test/Sencha/ExtJS/4/4/Components/CustomComponent/TestPlugin/app.js
@@ -0,0 +1,98 @@
+﻿Ext.Loader.setConfig({
+	enabled: true,
+	disableCaching: false
+});
+
+Ext.define("SimplePlugin", {
+	extend: "Ext.AbstractPlugin",
+	alias: "plugin.simpleplugin",
+
+    ptype: "simpleplugin",
+
+	constructor: function (config) {
+		if(window.console && console.log)
+			console.log("SimplePlugin.constructor(%o)", arguments);
+
+		this.callParent([config]);
+
+		return this;
+	},
+
+	init: function (cmp) {
+		if(window.console && console.log)
+			console.log("SimplePlugin.init(%o)", arguments);
+
+		var me = this;
+
+		cmp.addEvents({
+			"testEvent1": true,
+			"testEvent2": true
+		});
+
+		cmp.on("testEvent1", me.onTestEvent1, me);
+		cmp.on({
+			testEvent2: me.onTestEvent1,
+			scope: me
+		});
+	},
+
+	destroy: function () {
+		if(window.console && console.log)
+			console.log("SimplePlugin.destroy(%o)", arguments);
+	},
+
+	onTestEvent1: function () {
+		if(window.console && console.log)
+			console.log("SimplePlugin.onTestEvent1(%o)", arguments);
+	},
+
+	onTestEvent2: function () {
+		if(window.console && console.log)
+			console.log("SimplePlugin.onTestEvent2(%o)", arguments);
+	}
+});
+
+Ext.define("CustomPanel", {
+	extend: "Ext.panel.Panel",
+	alias : "widget.custompanel",
+
+	constructor: function(config) {
+		if(window.console && console.log)
+			console.log("CustomPanel.constructor(%o)", arguments);
+
+		this.callParent([config]);
+
+		return this;
+	},
+
+	initComponent: function() {
+		if(window.console && console.log)
+			console.log("CustomPanel.initComponent(%o)", arguments);
+
+		Ext.apply(this, {
+			plugins: ["simpleplugin"]
+		});
+
+		this.callParent();
+	}
+});
+
+Ext.onReady(function() {
+	if(window.console && console.clear)
+		console.clear();
+
+	if(window.console && console.log)
+		console.log("core: %s, extjs: %s", Ext.versions.core.version, Ext.versions.extjs.version);
+
+	var
+		p = Ext.create("CustomPanel", {
+			border: 50,
+			renderTo: Ext.getBody(),
+			listeners: {
+				render: function(panel, eOpts) {
+					if(window.console && console.log)
+						console.log("CustomPanel.render(%o)", arguments);
+				}
+			}
+		});
+});
