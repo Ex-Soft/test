8de714311fb3cf083adf648701c4307ca726fa31
Ex_Soft <4others2@gmail.com>
2018-07-04 00:03:41 +0300

    ExtJS

---
+++ JavaScript/test/Sencha/ExtJS/4/4/Components/Ext.tree.Panel/LoadTreePanel/app.js
@@ -0,0 +1,50 @@
+﻿Ext.Loader.setConfig({
+	enabled: true,
+	disableCaching: false
+});
+
+Ext.def
+
+Ext.onReady(function() {
+	Ext.QuickTips.init();
+
+	if(window.console && console.clear)
+		console.clear();
+
+	if(window.console && console.log)
+		console.log("core: %s, extjs: %s", Ext.versions.core.version, Ext.versions.extjs.version);
+
+	Ext.create("Ext.tree.Panel", {
+		title: "Ext.tree.Panel",
+		height: 500,
+		width: 900,
+		store: Ext.create("Ext.data.TreeStore", {
+			root: {
+				expanded: false,
+				id: "root",
+				text: "/"
+			},
+			proxy: {
+				type: "ajax",
+				url: "getData.php"
+			}
+		}),
+		rootVisible: true,
+		useArrows: true,
+		renderTo: Ext.getBody(),
+		dockedItems: [{
+			xtype: "toolbar",
+			dock: "top",
+			items: [{
+				text: "watch",
+				handler: function(btn, e) {
+					var
+						store = btn.up("treepanel").getStore();
+
+					if(store)
+						;
+				}
+			}]
+		}]
+	});
+});
---
+++ JavaScript/test/Sencha/ExtJS/4/4/Components/Ext.tree.Panel/LoadTreePanel/getData.php
@@ -0,0 +1,28 @@
+﻿<?php
+header("Content-type: application/json");
+
+$node = $_GET["node"];
+
+if (!is_numeric($node)) {
+    $data = array("success" => true, "children" => array(
+        array("id" => 1, "parentId" => $node, "text" => "Folder# 1"),
+        array("id" => 2, "parentId" => $node, "text" => "Folder# 2"),
+        array("id" => 3, "parentId" => $node, "text" => "Folder# 3"),
+        array("id" => 4, "parentId" => $node, "text" => "Folder# 4"),
+        array("id" => 5, "parentId" => $node, "text" => "Folder# 5")
+    ));
+} else {
+    $node = intval($node);
+    $isLeaf = $node > 10;
+
+    $data = array("success" => true, "message" => gettype($node), "children" => array(
+        array("id" => $node * 10 + 1, "parentId" => $node, "text" => ($isLeaf ? "File " : "Folder")."# ".strval($node * 10 + 1), "leaf" => $isLeaf),
+        array("id" => $node * 10 + 2, "parentId" => $node, "text" => ($isLeaf ? "File " : "Folder")."# ".strval($node * 10 + 2), "leaf" => $isLeaf),
+        array("id" => $node * 10 + 3, "parentId" => $node, "text" => ($isLeaf ? "File " : "Folder")."# ".strval($node * 10 + 3), "leaf" => $isLeaf),
+        array("id" => $node * 10 + 4, "parentId" => $node, "text" => ($isLeaf ? "File " : "Folder")."# ".strval($node * 10 + 4), "leaf" => $isLeaf),
+        array("id" => $node * 10 + 5, "parentId" => $node, "text" => ($isLeaf ? "File " : "Folder")."# ".strval($node * 10 + 5), "leaf" => $isLeaf)
+    ));
+}
+
+echo json_encode($data);
+?>
