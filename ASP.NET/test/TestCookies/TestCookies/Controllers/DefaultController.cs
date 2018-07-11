88e2c397bd97c81478dff54a9d82930b45d3ddea
Ex_Soft <4others2@gmail.com>
2018-07-11 23:37:07 +0300

    ASP.NET

---
+++ ASP.NET/test/TestCookies/TestCookies/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace TestCookies.Controllers
+{
+    [RoutePrefix("api/default")]
+    public class DefaultController : ApiController
+    {
+        public HttpResponseMessage GetSmth()
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent("hello", Encoding.Unicode);
+            response.Headers.Add("Set-Cookie", "cookie1=cookie1");
+
+            return response;
+        }
+    }
+}
---
+++ ASP.NET/test/TestCookies/TestCookies/Scripts/app.js
@@ -0,0 +1,44 @@
+﻿function initXMLHTTPRequest() {
+    var
+        xRequest = null;
+
+    if (window.XMLHttpRequest)
+        xRequest = new XMLHttpRequest();
+    else if (window.ActiveXObject) {
+        try {
+            xRequest = new ActiveXObject("Msxml2.XMLHTTP");
+        }
+        catch (e) {
+            try {
+                xRequest = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            catch (e) {
+                alert(e.name + ": " + e.message);
+            }
+        }
+    }
+
+    return (xRequest);
+}
+
+function doRequest() {
+    var req;
+
+    if (!(req = initXMLHTTPRequest()))
+        return;
+
+    req.onreadystatechange = function () {
+        if (req.readyState == 4) {
+            if (req.status == 200)
+                process(req);
+            else {
+                if (window.console && console.log)
+                    console.log("status=" + req.status + "\r\nstatusText=\"" + req.statusText + "\"\r\nresponseText=\"" + req.responseText + "\"");
+            }
+            req = null;
+        }
+    };
+    req.open("GET", "api/default", true);
+    //req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    req.send();
+}
