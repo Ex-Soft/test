6ee34e42bd9a6026f866b95f73ee9527b5f7cc9b
Ex_Soft <4others2@gmail.com>
2018-07-16 08:12:49 +0300

    ASP.NET

--- ASP.NET/test/RouteDebugger/RouteDebugger/Controllers/Test1Controller.cs
+++ ASP.NET/test/RouteDebugger/RouteDebugger/Controllers/Test1Controller.cs
@@ -5,11 +5,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
 
 namespace RouteDebugger.Controllers
 {
     public class Test1Controller : ApiController
     {
+        public HttpResponseMessage Get()
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, "value");
+            response.Content = new StringContent("hello", Encoding.UTF8);
+            return response;
+        }
+
+        public void Post()
+        {
+
+        }
     }
 }
