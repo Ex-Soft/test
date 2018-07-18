85b5c24459d415fae4b1bccffeb23fd8391f1d18
Ex_Soft <4others2@gmail.com>
2018-07-18 15:09:54 +0300

    ExtJS

---
+++ JavaScript/test/Sencha/ExtJS/4/4/data/store/TestLoad/app.js
@@ -0,0 +1,61 @@
+﻿Ext.Loader.setConfig({
+    enabled: true,
+    disableCaching: false
+});
+
+Ext.define("TestModel", {
+    extend: "Ext.data.Model",
+    idProperty: "id",
+    fields: [
+        { name: "id", type: "int" },
+        { name: "value" }
+    ]
+});
+
+Ext.define("TestStore", {
+    extend: "Ext.data.Store",
+    model: "TestModel",
+    //autoLoad: true,
+    proxy: {
+        type: "ajax",
+        url: "Data.xml",
+        reader: {
+            type: "xml",
+            record: "row",
+            idProperty: "id",
+            totalProperty: "total",
+            successProperty: "success",
+            messageProperty: "message"
+        }
+    },
+    listeners: {
+        beforeload: function(store, operation, eOpts) {
+            if (window.console && console.log)
+                console.log("Store.beforeload");
+
+            // return false;
+        },
+        load: function (store, records, successful, operation) {
+            if (window.console && console.log)
+                console.log("Store.load: successful=%s records=%o", successful, records);
+        }
+    }
+});
+
+Ext.onReady(function() {
+    if(window.console && console.clear)
+        console.clear();
+
+    if(window.console && console.log)
+        console.log("core: %s, extjs: %s", Ext.versions.core.version, Ext.versions.extjs.version);
+
+	Ext.QuickTips.init();
+
+	var
+		testStore = Ext.create("TestStore");
+
+	if(window.console && console.log)
+		console.log("%o", testStore);
+
+	testStore.load();
+});
