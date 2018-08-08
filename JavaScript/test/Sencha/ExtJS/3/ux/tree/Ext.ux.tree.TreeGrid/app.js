834e43ed14ae77c2e99d0d39f4ea10e58e766798
Ex_Soft <4others2@gmail.com>
2018-08-08 13:04:15 +0300

    ExtJS

--- JavaScript/test/Sencha/ExtJS/3/ux/tree/Ext.ux.tree.TreeGrid/app.js
+++ JavaScript/test/Sencha/ExtJS/3/ux/tree/Ext.ux.tree.TreeGrid/app.js
@@ -11,6 +11,9 @@ Ext.onReady(function() {
 
 	var
 		treeGrid = new Ext.ux.tree.TreeGrid({
+			title: 'Ext.ux.tree.TreeGrid',
+			width: 500,
+			height: 300,
 			renderTo: Ext.getBody(),
 			columns: [{
 				dataIndex: "task",
@@ -25,7 +28,60 @@ Ext.onReady(function() {
 				header: "Assigned To",
 				width: 150
 			}],
-			dataUrl: "treegrid-data.json"
+			dataUrl: "treegrid-data.json",
+			listeners: {
+				dblclick: function (node, e) {
+					if (window.console && console.log)
+						console.log("dblclick(%o)", arguments);
+
+					treeEditor.editNode = node;
+					treeEditor.startEdit(node.ui.textNode);
+				}
+			}
+		}),
+		treeEditor = new Ext.tree.TreeEditor(treeGrid, {
+				allowBlank: false
+				/*, validator: function (value) {
+					if (window.console && console.log)
+						console.log("validator(%o)", arguments);
+					return "blah-blah-blah";
+				}*/
+				, listeners: {
+					afterrender: function (editor) {
+						if (window.console && console.log)
+							console.log("afterrender(%o)", arguments);
+
+						var
+							root,
+							ui,
+							el;
+
+						if (!(root = treeGrid.getRootNode())
+							|| !(ui = root.getUI())
+							|| !(el = ui.getEl())
+							|| !el.parentNode)
+							return;
+
+						//treeGrid.el.appendChild(editor.el);
+						//Ext.fly(el.parentNode).appendChild(editor.el);
+					}
+				}
+			}, {
+			cancelOnEsc: true,
+			completeOnEnter: true,
+			selectOnFocus: true,
+			//allowBlank: false,
+			validator: function () {
+				if (window.console && console.log)
+					console.log("validator(%o)", arguments);
+			},
+			listeners: {
+				complete: function(treeEditor, oldValue, newValue) {
+					if (window.console && console.log)
+						console.log("complete(%o)", arguments);
+
+				}
+			}
 		});
 });
 
