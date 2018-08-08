ed017be1e28873dc520a2316dfb911f0fe884219
Ex_Soft <4others2@gmail.com>
2018-08-08 21:32:00 +0300

    Java

--- java/test/www/ftp/TestFtp/src/main/java/TestFtp/TestFtp.java
+++ java/test/www/ftp/TestFtp/src/main/java/TestFtp/TestFtp.java
@@ -1,20 +1,73 @@
-/*
+package TestFtp;
 
-Building Java Projects with Maven
+import java.io.IOException;
 
-https://spring.io/guides/gs/maven/
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 
- */
-
-package hello;
-
-import org.joda.time.LocalTime;
-
-public class HelloWorld {
+public class TestFtp {
     public static void main(String[] args) {
-        LocalTime currentTime = new LocalTime();
-        System.out.println("The current local time is: " + currentTime);
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+        String server = "speedtest.tele2.net";
+        String user = "anonymous";
+        String pass = "password";
+
+        FTPClient ftpClient = new FTPClient();
+
+        try {
+            ftpClient.connect(server);
+            ftpClient.login(user, pass);
+
+            // use local passive mode to pass firewall
+            ftpClient.enterLocalPassiveMode();
+
+            // get details of a file or directory
+            String remoteFilePath = "1000GB.zip";
+
+            FTPFile ftpFile = ftpClient.mlistFile(remoteFilePath);
+            if (ftpFile != null) {
+                String name = ftpFile.getName();
+                long size = ftpFile.getSize();
+                String timestamp = ftpFile.getTimestamp().getTime().toString();
+                String type = ftpFile.isDirectory() ? "Directory" : "File";
+
+                System.out.println("Name: " + name);
+                System.out.println("Size: " + size);
+                System.out.println("Type: " + type);
+                System.out.println("Timestamp: " + timestamp);
+            } else {
+                System.out.println("The specified file/directory may not exist!");
+            }
+
+            FTPFile[] ftpFiles = ftpClient.listFiles();
+
+            if (ftpFiles != null) {
+                for (int i = 0; i < ftpFiles.length; ++i) {
+                    ftpFile = ftpFiles[i];
+
+                    String name = ftpFile.getName();
+                    long size = ftpFile.getSize();
+                    String timestamp = ftpFile.getTimestamp().getTime().toString();
+                    String type = ftpFile.isDirectory() ? "Directory" : "File";
+
+                    System.out.println("Name: " + name);
+                    System.out.println("Size: " + size);
+                    System.out.println("Type: " + type);
+                    System.out.println("Timestamp: " + timestamp);
+                }
+            }
+            ftpClient.logout();
+            ftpClient.disconnect();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (ftpClient.isConnected()) {
+                try {
+                    ftpClient.disconnect();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
     }
 }
