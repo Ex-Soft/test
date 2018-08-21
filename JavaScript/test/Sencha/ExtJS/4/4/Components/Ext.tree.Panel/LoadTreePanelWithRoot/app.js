6b5061ca0ebcb677e9c0f58224f4f003381fd1d4
Ex_Soft <4others2@gmail.com>
2018-08-21 18:19:46 +0300

    ExtJS

---
+++ JavaScript/test/Sencha/ExtJS/4/4/Components/Ext.tree.Panel/LoadTreePanelWithRoot/app.js
@@ -0,0 +1,62 @@
+﻿Ext.Loader.setConfig({
+	enabled: true,
+	disableCaching: false
+});
+
+Ext.onReady(function() {
+	Ext.QuickTips.init();
+
+	if(window.console && console.clear)
+		console.clear();
+
+	if(window.console && console.log)
+		console.log("core: %s, extjs: %s", Ext.versions.core.version, Ext.versions.extjs.version);
+
+	var tp = Ext.create("Ext.tree.Panel", {
+		title: "Ext.tree.Panel",
+		rootVisible: false,
+		useArrows: true,
+		height: 500,
+		width: 900,
+		renderTo: Ext.getBody(),
+		store: Ext.create("Ext.data.TreeStore", {
+			proxy: {
+				type: "ajax",
+				url: "getData.php"
+				//url: "getDataWithRoot.php"
+			},
+			listeners: {
+				beforeexpand: function () {
+					if (window.console && console.log)
+						console.log("Ext.data.TreeStore.beforeexpand(%o)", arguments);
+				},
+				expand: function (parent, records, suppressEvent, listeners) {
+					if (window.console && console.log)
+						console.log("Ext.data.TreeStore.expand(%o)", arguments);
+				},
+				load: function () {
+					if (window.console && console.log)
+						console.log("Ext.data.TreeStore.load(%o)", arguments);
+				}
+			}
+		}),
+		dockedItems: [{
+			xtype: "toolbar",
+			dock: "top",
+			items: [{
+				text: "getRootNode()",
+				handler: function(btn, e) {
+					var
+						treePanel,
+						rootNode;
+
+					if (!(treePanel = btn.up("treepanel"))) {
+						return;
+					}
+
+					rootNode = treePanel.getRootNode();
+				}
+			}]
+		}]
+	});
+});
---
+++ JavaScript/test/Sencha/ExtJS/4/4/Components/Ext.tree.Panel/LoadTreePanelWithRoot/getDataWithRoot.php
@@ -0,0 +1,15 @@
+﻿<?php
+header("Content-type: application/json");
+
+$node = $_GET["node"];
+
+if (!is_numeric($node)) {
+    $data = array("success" => true, "children" => array("text"=>"Root (loaded)", "parentId" => "", "leaf" => false, "children" => array(
+        array("id" => 1, "parentId" => $node, "text" => "Folder# 1", "leaf" => false),
+        array("id" => 2, "parentId" => $node, "text" => "Folder# 2", "leaf" => false),
+        array("id" => 3, "parentId" => $node, "text" => "Folder# 3", "leaf" => false)
+    )));
+}
+
+echo json_encode($data);
+?>
