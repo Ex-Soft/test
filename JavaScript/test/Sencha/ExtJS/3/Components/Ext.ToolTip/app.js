585116492f3552465c81fdfdf65f303c6722d689
Ex_Soft <4others2@gmail.com>
2018-08-22 18:11:34 +0300

    ExtJS

---
+++ JavaScript/test/Sencha/ExtJS/3/Components/Ext.ToolTip/app.js
@@ -0,0 +1,40 @@
+﻿Ext.BLANK_IMAGE_URL = "../../../../../../Sencha/ExtJS/ExtJS3/ExtJS3/resources/images/default/s.gif";
+
+Ext.onReady(function() {
+	var
+		body = Ext.getBody(),
+		toolTip = new Ext.ToolTip({
+			target: Ext.getBody(),
+			renderTo: Ext.getBody(),
+			anchor: "top",
+			autoHide: false,
+			html: "Ext.ToolTip.html"
+		});
+
+	body.on("resize", function (w, h) {
+		if (window.console && console.log)
+			console.log("body.resize(%o)", arguments);
+	});
+
+	body.on("onresize", function (w, h) {
+		if (window.console && console.log)
+			console.log("body.onresize(%o)", arguments);
+	});
+	
+	toolTip.on("bodyresize", function (w, h) {
+		if (window.console && console.log)
+			console.log("Ext.ToolTip.bodyresize(%o)", arguments);
+	});
+
+	toolTip.target.on("resize", function (w, h) {
+		if (window.console && console.log)
+			console.log("Ext.ToolTip.target.resize(%o)", arguments);
+	});
+
+	toolTip.target.on("onresize", function (w, h) {
+		if (window.console && console.log)
+			console.log("Ext.ToolTip.target.onresize(%o)", arguments);
+	});
+
+	toolTip.show();
+});
---
+++ JavaScript/test/Sencha/ExtJS/4/4/Components/Ext.tip/Ext.tip.ToolTip/app.js
@@ -0,0 +1,11 @@
+﻿Ext.onReady(function() {
+	var toolTip = Ext.create("Ext.tip.ToolTip", {
+		target: Ext.getBody(),
+		renderTo: Ext.getBody(),
+		anchor: "top",
+		autoHide: false,
+		html: "Ext.tip.ToolTip.html"
+	});
+
+	toolTip.show();
+});
