42c39d528336864dd4951adc6e6de0daec3b40df
Ex_Soft <4others2@gmail.com>
2018-08-24 12:00:11 +0300

    cpp

--- cpp/test/classes/TestVTABLE/main.cpp
+++ cpp/test/classes/TestVTABLE/main.cpp
@@ -1,35 +1,37 @@
 // http://www.sql.ru/forum/1293891/virtual-methods
 
+#include <iostream>
+
 class A {
-public :
+public:
     virtual void m() {
-        std::cout << "A\n";
+        std::cout << "A::m()" << std::endl;
     }
 
     virtual ~A() {
-        std::cout << "~A\n";
+        std::cout << "A::~A()" << std::endl;
     }
 };
 
 class B : public A {
-public :
+public:
     void m() override {
-        std::cout << "B\n";
+        std::cout << "B::m()" << std::endl;
     }
 
     virtual ~B() {
-        std::cout << "~B\n";
+        std::cout << "B::~B()" << std::endl;
     }
 };
 
 class C : public B {
-public :
+public:
     void m() override {
-        std::cout << "C\n";
+        std::cout << "C::m()" << std::endl;
     }
 
     virtual ~C() {
-        std::cout << "~C\n";
+        std::cout << "C::~C()" << std::endl;
     }
 };
 
@@ -39,8 +41,8 @@ int main() {
     delete o;
 
     typedef void (*PMethodProto)(void*);  // -- (void*) - неявный параметр this
-    A* a = new A();
-    C* c = new C();
+    A *a = new A();
+    C *c = new C();
     PMethodProto **pVTBL = reinterpret_cast<PMethodProto**>(c);
     (*pVTBL)[0](pVTBL);
     *pVTBL = *reinterpret_cast<PMethodProto**>(a);
