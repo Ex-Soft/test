6e787ad70adb85312015881a909c6236a6d87416
Ex_Soft <4others2@gmail.com>
2018-08-29 18:11:40 +0300

    ExtJS

---
+++ JavaScript/test/Sencha/ExtJS/3/ux/form/SuperBoxSelect/app.js
@@ -0,0 +1,33 @@
+﻿Ext.BLANK_IMAGE_URL = "../../../../../../../Sencha/ExtJS/ExtJS3/ExtJS3/resources/images/default/s.gif";
+
+Ext.onReady(function() {
+	Ext.QuickTips.init();
+
+	if(window.console && console.clear)
+		console.clear();
+
+	if(window.console && console.log)
+		console.log(Ext.version);
+
+	var
+		cb = new Ext.ux.form.SuperBoxSelect({
+			store: {
+				xtype: "arraystore",
+				fields: ["id", "value"],
+				data: [
+					[ 1, "value# 1" ]
+				]
+			},
+			valueField: "id",
+			displayField: "value"
+		}),
+		p = new Ext.Panel({
+			border: 50,
+			items: [ cb ],
+			renderTo: Ext.getBody(),
+			tbar: {
+				items: [{
+				}]
+			}
+		});
+});
