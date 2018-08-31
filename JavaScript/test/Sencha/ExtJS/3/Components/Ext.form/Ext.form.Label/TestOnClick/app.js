1beeced7b9d3228ca12a5f011fdb97b6c2dd4d21
Ex_Soft <4others2@gmail.com>
2018-08-31 13:48:16 +0300

    ExtJS

---
+++ JavaScript/test/Sencha/ExtJS/3/Components/Ext.form/Ext.form.Label/TestOnClick/app.js
@@ -0,0 +1,33 @@
+﻿Ext.onReady(function() {
+	var
+		p = new Ext.Panel({
+			items: [{
+				xtype: "label",
+				text: " onClick ",
+				listeners: {
+					render: function(label, eOpts) {
+						this.getEl().on("click", this.onClick);
+					}
+				},
+
+				onClick: function(e, htmlElement, eOpts) {
+					if(window.console && console.log)
+						console.log("onClick(%o)", arguments);
+				}
+			}, {
+				xtype: "label",
+				text: " onDblClick ",
+				listeners: {
+					render: function(label, eOpts) {
+						Ext.EventManager.addListener(this.getEl().dom, "dblclick", this.onDblClick, this);
+					}
+				},
+
+				onDblClick: function(e, htmlElement, eOpts) {
+					if(window.console && console.log)
+						console.log("onDblClick(%o)", arguments);
+				}
+			}],
+			renderTo: Ext.getBody()
+		});
+});
---
+++ JavaScript/test/Sencha/ExtJS/3/Components/Ext.form/Ext.form.NumberField/TestValidation/app.js
@@ -0,0 +1,84 @@
+﻿Ext.BLANK_IMAGE_URL = "../../../../../../../../Sencha/ExtJS/ExtJS3/ExtJS3/resources/images/default/s.gif";
+
+Ext.ns("App");
+
+App.CustomNumberField = Ext.extend(Ext.form.NumberField, {
+	validate: function () {
+		if (window.console && console.log)
+			console.log("CustomNumberField.validate(%o)", arguments);
+
+		return App.CustomNumberField.superclass.validate.apply(this, arguments);
+	},
+
+	isValid: function () {
+		if (window.console && console.log)
+			console.log("CustomNumberField.isValid(%o)", arguments);
+
+		return App.CustomNumberField.superclass.isValid.apply(this, arguments);
+	},
+
+	validateValue: function () {
+		if (window.console && console.log)
+			console.log("CustomNumberField.validateValue(%o)", arguments);
+
+		return App.CustomNumberField.superclass.validateValue.apply(this, arguments);
+	},
+
+	getErrors: function () {
+		if (window.console && console.log)
+			console.log("CustomNumberField.getErrors(%o)", arguments);
+
+		return App.CustomNumberField.superclass.getErrors.apply(this, arguments);
+	}
+});
+
+Ext.reg("customnumberfield", App.CustomNumberField);
+
+Ext.onReady(function() {
+	Ext.QuickTips.init();
+
+	if(window.console && console.clear)
+		console.clear();
+
+	if(window.console && console.log)
+		console.log(Ext.version);
+
+	var
+		p = new Ext.Panel({
+			items: [{
+				xtype: "form",
+				items: [{
+					xtype: "customnumberfield",
+					minValue: 10,
+					maxValue: 20
+				}]
+			}],
+			tbar: [{
+				text: "isValid",
+				handler: function (btn) {
+					var panel,
+						formPanels,
+						form,
+						numberFields,
+						numberField;
+
+					if (!(panel = btn.findParentByType("panel"))
+						|| Ext.isEmpty(formPanels = panel.findByType("form"))
+						|| !(form = formPanels[0].getForm())
+						|| Ext.isEmpty(numberFields = formPanels[0].findByType("customnumberfield")))
+						return;
+
+					numberField = numberFields[0];
+
+					if(window.console && console.log)
+						console.log("NumberField.isValid()");
+					numberField.isValid(); // isValid() -> validateValue() -> getErrors()
+
+					if(window.console && console.log)
+						console.log("Form.isValid()");
+					form.isValid(); // form.isValid() -> field.validate()-> field.validateValue() -> field.getErrors()
+				}
+			}],
+			renderTo: Ext.getBody()
+		});
+});
