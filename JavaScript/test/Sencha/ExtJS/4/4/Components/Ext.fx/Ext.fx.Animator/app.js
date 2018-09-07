1ded8d328dae25f0df31005a9191e228d5b9a33d
Ex_Soft <4others2@gmail.com>
2018-09-07 12:24:47 +0300

    ExtJS

---
+++ JavaScript/test/Sencha/ExtJS/4/4/Components/Ext.fx/Ext.fx.Animator/app.js
@@ -0,0 +1,62 @@
+﻿Ext.Loader.setConfig({
+	enabled: true,
+	disableCaching: false
+});
+
+Ext.onReady(function() {
+	if(window.console && console.clear)
+		console.clear();
+
+	if(window.console && console.log)
+		console.log("core: %s, extjs: %s", Ext.versions.core.version, Ext.versions.extjs.version);
+
+	var
+		animator = Ext.create("Ext.fx.Animator", {
+			target: "ball",
+			duration: 5000,
+			keyframes: {
+				0: {
+					y: 50,
+					opacity: 1,
+					backgroundColor: "FF0000"
+				},
+				20: {
+					y: 300,
+					x: 30,
+					opacity: 0.5
+				},
+				40: {
+					y: 175,
+					x: 130,
+					backgroundColor: "0000FF"
+				},
+				60: {
+					y: 80,
+					opacity: 0.3
+				},
+				80: {
+					width: 200,
+					y: 200
+				},
+				100: {
+					y: 300,
+					opacity: 1,
+					backgroundColor: "00FF00"
+				}
+			},
+			listeners: {
+				afteranimate: function(panel, eOpts) {
+					if(window.console && console.log)
+						console.log("Ext.fx.Animator.afteranimate(%o)", arguments);
+				},
+				beforeanimate: function(panel, eOpts) {
+					if(window.console && console.log)
+						console.log("Ext.fx.Animator.beforeanimate(%o)", arguments);
+				},
+				keyframe: function(panel, eOpts) {
+					if(window.console && console.log)
+						console.log("Ext.fx.Animator.keyframe(%o)", arguments);
+				}
+			}
+		});
+});
---
+++ JavaScript/test/Sencha/ExtJS/4/4/Components/Ext.fx/Ext.fx/app.js
@@ -0,0 +1,25 @@
+﻿Ext.Loader.setConfig({
+	enabled: true,
+	disableCaching: false
+});
+
+Ext.onReady(function() {
+	if(window.console && console.clear)
+		console.clear();
+
+	if(window.console && console.log)
+		console.log("core: %s, extjs: %s", Ext.versions.core.version, Ext.versions.extjs.version);
+
+	Ext.get("div1").fadeOut();
+	window.setTimeout(function() {
+		Ext.get("div1").fadeIn();
+	}, 3000);
+
+	Ext.get("div2").fadeOut({endOpacity:0.25});
+	window.setTimeout(function() {
+		Ext.get("div2").fadeIn({endOpacity: 0.75});
+	}, 3000);
+
+	Ext.get("div3").frame();
+	Ext.get("div4").frame("ff0000", 3);
+});
