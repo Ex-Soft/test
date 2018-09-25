757319d818098a92f011c36d416356e1e9ef46f8
Ex_Soft <4others2@gmail.com>
2018-09-25 05:52:31 +0300

    java

--- java/test/lang/TestReflection/src/TestReflection.java
+++ java/test/lang/TestReflection/src/TestReflection.java
@@ -1,7 +1,19 @@
+import java.util.*;
+
 public class TestReflection {
     public static void main(String[] args)
     {
         Class testClass = TestClass.class;
-        System.out.println(testClass.getName());
+        String className = testClass.getName();
+        System.out.println(className);
+        TestClass tc1 = (TestClass)Class.forName(className);
+
+        Date dt1 = new Date();
+        Class dateClass = dt1.getClass();
+        System.out.println(className = dateClass.getName());
+        Date dt2 = (Date)Class.forName(className);
+
+        Class doubleArrayClass = Double[].class;
+        System.out.println(className = doubleArrayClass.getName());
     }
 }
