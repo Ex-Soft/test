3e789431cd7734ef3cc4ea5af35d23fabc63936e
Ex_Soft <4others2@gmail.com>
2018-10-04 23:12:17 +0300

    ExtJS

--- JavaScript/test/Sencha/ExtJS/3/ux/tree/Ext.ux.tree.TreeGrid/app.js
+++ JavaScript/test/Sencha/ExtJS/3/ux/tree/Ext.ux.tree.TreeGrid/app.js
@@ -1,4 +1,4 @@
-Ext.BLANK_IMAGE_URL="../../../../../../../Sencha/ExtJS/ExtJS3/ExtJS3.3.1/resources/images/default/s.gif";
+Ext.BLANK_IMAGE_URL="../../../../../../../Sencha/ExtJS/ExtJS3/ExtJS3/resources/images/default/s.gif";
 
 Ext.onReady(function() {
 	Ext.QuickTips.init();
@@ -37,6 +37,94 @@ Ext.onReady(function() {
 					treeEditor.editNode = node;
 					treeEditor.startEdit(node.ui.textNode);
 				}
+			},
+			tbar: {
+				items: [{
+					text: "UI (animate)",
+					handler: function (btn, e) {
+						var treeGrid;
+
+						if (!(treeGrid = btn.findParentByType("treegrid")))
+							return;
+
+						treeGrid.expandByUI(treeGrid, true);
+					}
+				}, {
+					text: "UI (!animate)",
+					handler: function (btn, e) {
+						var treeGrid;
+
+						if (!(treeGrid = btn.findParentByType("treegrid")))
+							return;
+
+						treeGrid.expandByUI(treeGrid, false);
+					}
+				}, {
+					text: "UI.getAnchor() (animate)",
+					handler: function (btn, e) {
+						var treeGrid;
+
+						if (!(treeGrid = btn.findParentByType("treegrid")))
+							return;
+
+						treeGrid.expandByAnchor(treeGrid, true);
+					}
+				}, {
+					text: "UI.getAnchor() (!animate)",
+					handler: function (btn, e) {
+						var treeGrid;
+
+						if (!(treeGrid = btn.findParentByType("treegrid")))
+							return;
+
+						treeGrid.expandByAnchor(treeGrid, false);
+					}
+				}]
+			},
+			expandByUI: function(treeGrid, anim) {
+				var root;
+
+				if (!treeGrid
+					|| !(root = treeGrid.getRootNode()))
+					return;
+
+				root.childNodes[1].expand(false, anim, function (node) {
+					if (window.console && console.log)
+						console.log("expand(%o)", arguments);
+
+					node.childNodes[2].expand(false, anim, function (node) {
+						if (window.console && console.log)
+							console.log("expand(%o)", arguments);
+
+						var dest = node.childNodes[1],
+							ui = dest.getUI();
+
+						ui.focus();
+					});
+				});
+			},
+			expandByAnchor: function(treeGrid, anim) {
+				var root;
+
+				if (!treeGrid
+					|| !(root = treeGrid.getRootNode()))
+					return;
+
+				root.childNodes[1].expand(false, anim, function (node) {
+					if (window.console && console.log)
+						console.log("expand(%o)", arguments);
+
+					node.childNodes[2].expand(false, anim, function (node) {
+						if (window.console && console.log)
+							console.log("expand(%o)", arguments);
+
+						var dest = node.childNodes[1],
+							ui = dest.getUI(),
+							anchor = ui.getAnchor();
+
+						anchor.focus();
+					});
+				});
 			}
 		}),
 		treeEditor = new Ext.tree.TreeEditor(treeGrid, {
