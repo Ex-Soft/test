10ad6e581d2500d973e845e1c019c79658929e79
Ex_Soft <4others2@gmail.com>
2018-10-04 21:35:59 +0300

    java

--- java/test/lang/TestReflection/src/TestReflection.java
+++ java/test/lang/TestReflection/src/TestReflection.java
@@ -3,17 +3,25 @@ import java.util.*;
 public class TestReflection {
     public static void main(String[] args)
     {
-        Class testClass = TestClass.class;
-        String className = testClass.getName();
-        System.out.println(className);
-        TestClass tc1 = (TestClass)Class.forName(className);
+        try {
+            Class _class = TestClass.class;
+            String className = _class.getName();
+            System.out.println(className);
+            _class = Class.forName(className);
+            TestClass testClass = (TestClass)Class.forName(className).newInstance();
 
-        Date dt1 = new Date();
-        Class dateClass = dt1.getClass();
-        System.out.println(className = dateClass.getName());
-        Date dt2 = (Date)Class.forName(className);
+            Date dt1 = new Date();
+            _class = dt1.getClass();
+            System.out.println(className = _class.getName());
+            _class = Class.forName(className);
+            dt1 = (Date)Class.forName(className).newInstance();
 
-        Class doubleArrayClass = Double[].class;
-        System.out.println(className = doubleArrayClass.getName());
+            Class doubleArrayClass = Double[].class;
+            System.out.println(className = doubleArrayClass.getName());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
