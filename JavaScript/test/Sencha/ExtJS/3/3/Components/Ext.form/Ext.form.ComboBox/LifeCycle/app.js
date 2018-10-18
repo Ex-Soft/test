fd7aab897ebabf3d0d0ca4ce2c58a9c47d637454
Ex_Soft <4others2@gmail.com>
2018-10-18 16:32:07 +0300

    ExtJS

--- JavaScript/test/Sencha/ExtJS/3/3/Components/Ext.form/Ext.form.ComboBox/LifeCycle/app.js
+++ JavaScript/test/Sencha/ExtJS/3/3/Components/Ext.form/Ext.form.ComboBox/LifeCycle/app.js
@@ -1,4 +1,4 @@
-﻿Ext.BLANK_IMAGE_URL="../../../../../../../../Sencha/ExtJS/ExtJS3/ExtJS3/resources/images/default/s.gif";
+﻿Ext.BLANK_IMAGE_URL="../../../../../../../../../Sencha/ExtJS/ExtJS3/ExtJS3/resources/images/default/s.gif";
 
 Ext.ns("Test");
 
