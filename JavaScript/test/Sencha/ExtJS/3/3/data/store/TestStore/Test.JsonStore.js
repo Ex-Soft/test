4ce51dcf3fb6e0fdf77b73573218a05dcca190a9
Ex_Soft <4others2@gmail.com>
2018-10-22 17:55:05 +0300

    ExtJS

--- JavaScript/test/Sencha/ExtJS/3/3/data/store/TestStore/Test.JsonStore.js
+++ JavaScript/test/Sencha/ExtJS/3/3/data/store/TestStore/Test.JsonStore.js
@@ -4,7 +4,8 @@ function TestJsonStore()
         data = [
             { id: "1", value: "Rec# 1" },
             { id: "2", value: "Rec# 2" },
-            { id: "3", value: "Rec# 3" }
+			{ id: "3", value: "Rec# 3" },
+			{ id: "4", value: "tag" }
         ],
 		store = new Ext.data.JsonStore({
             autoDestroy: true,
@@ -18,10 +19,14 @@ function TestJsonStore()
 						console.info("Ext.data.JsonStore.load(%o)", arguments);
 				}
 			}
-		});
+		}),
+		field;
+
+	field = store.fields.get("value");
 
 	store.loadData(data);
 
+	store.filter("value", new RegExp("(?!^tag$)(^.*$)", "i"));
 	store.filter("id", 1);
 	store.filter({ fn: function(record) { return record.get("id") == 2; }});
 }
