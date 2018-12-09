e2920c913b5561de8f2c9e8afebc54ced4bced51
Ex_Soft <4others2@gmail.com>
2018-12-09 22:10:17 +0200

    c#

--- c#/test/lang/LINQ/TestPerformance/main.cs
+++ c#/test/lang/LINQ/TestPerformance/main.cs
@@ -17,15 +17,65 @@ namespace TestPerformance
                 new TestClassIComparableFull(2)
             };
 
-            Debug.WriteLine("sort");
-            var sort = arr.OrderByDescending(item => item).ToArray();
+            //Debug.WriteLine("sort");
+            //var sort = arr.OrderByDescending(item => item).ToArray();
             Debug.WriteLine("sort -> distinct");
             var sortDistinct = arr.OrderByDescending(item => item).Distinct().ToArray();
+/*
+sort -> distinct
 
-            Debug.WriteLine("distinct");
-            var distinct = arr.Distinct().ToArray();
+int CompareTo(object {FiledInt: 5})
+int CompareTo(object {FiledInt: -1})
+int CompareTo(object {FiledInt: 2})
+int CompareTo(object {FiledInt: 5})
+int CompareTo(object {FiledInt: 3})
+int CompareTo(object {FiledInt: 3})
+int CompareTo(object {FiledInt: -1})
+int CompareTo(object {FiledInt: 5})
+int CompareTo(object {FiledInt: 3})
+int CompareTo(object {FiledInt: 5})
+int CompareTo(object {FiledInt: 5})
+int CompareTo(object {FiledInt: 5})
+int CompareTo(object {FiledInt: 5})
+int CompareTo(object {FiledInt: 5})
+
+override int GetHashCode(5)
+override int GetHashCode(5)
+override bool Equals(object {FiledInt: 5})
+operator == (TestClassIComparableFull {FiledInt: 5}, TestClassIComparableFull {FiledInt: 5})
+override int GetHashCode(3)
+override int GetHashCode(2)
+override int GetHashCode(0)
+override int GetHashCode(-1)
+
+*/
+
+            //Debug.WriteLine("distinct");
+            //var distinct = arr.Distinct().ToArray();
             Debug.WriteLine("distinct -> sort");
             var distinctSort = arr.Distinct().OrderByDescending(item => item).ToArray();
+/*
+distinct -> sort
+
+override int GetHashCode(5)
+override int GetHashCode(-1)
+override int GetHashCode(0)
+override int GetHashCode(3)
+override int GetHashCode(5)
+override bool Equals(object {FiledInt: 5})
+operator == (TestClassIComparableFull {FiledInt: 5}, TestClassIComparableFull {FiledInt: 5})
+override int GetHashCode(2)
+
+int CompareTo(object {FiledInt: 5})
+int CompareTo(object {FiledInt: -1})
+int CompareTo(object {FiledInt: 2})
+int CompareTo(object {FiledInt: 3})
+int CompareTo(object {FiledInt: -1})
+int CompareTo(object {FiledInt: 5})
+int CompareTo(object {FiledInt: 3})
+int CompareTo(object {FiledInt: 3})
+
+*/
         }
     }
 }
