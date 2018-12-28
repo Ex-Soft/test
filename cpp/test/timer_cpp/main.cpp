ac557f565397a71f46985a3a7564d92f8c9c19a8
Ex_Soft <4others2@gmail.com>
2018-12-28 23:36:21 +0200

    cpp

---
+++ cpp/test/timer_cpp/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "timer.h"
+
+int main(int argc, char **argv)
+{
+    Timer t = Timer();
+ 
+    t.setInterval([&]() {
+        std::cout << "Hey.. After each 1s..." << std::endl;
+    }, 1000); 
+ 
+    t.setTimeout([&]() {
+        std::cout << "Hey.. After 5.2s. But I will stop the timer!" << std::endl;
+        t.stop();
+    }, 5200);
+
+    std::cout << "I am Timer" << std::endl;
+
+    while(true);
+
+    return 0;
+}
