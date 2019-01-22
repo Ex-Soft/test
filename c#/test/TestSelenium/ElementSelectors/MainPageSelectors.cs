2c5d5a83f896b13981de5df0c98fd0b80ba55e71
Ex_Soft <4others2@gmail.com>
2019-01-22 16:39:19 +0200

    c#

--- c#/test/TestSelenium/ElementSelectors/MainPageSelectors.cs
+++ c#/test/TestSelenium/ElementSelectors/MainPageSelectors.cs
@@ -7,5 +7,6 @@ namespace TestSelenium.ElementSelectors
     {
         public static Func<string, By> ByIdSelector = id => By.XPath($"//*[@id='{id}']");
         public static By InputSelector = By.XPath("//input[@type='text']");
+        public static By RootSelector = By.CssSelector(".component-content .tree-wrapper .tree-node.tree-root");
     }
 }
--- c#/test/TestSelenium/main.cs
+++ c#/test/TestSelenium/main.cs
@@ -1,4 +1,7 @@
 ﻿using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using TestSelenium.ElementSelectors;
 using TestSelenium.Extensions;
 using TestSelenium.Pages;
@@ -18,8 +21,34 @@ namespace TestSelenium
                 MainPage mainPage = new MainPage(webDriver);
 
                 IWebElement
+                    root = MainPageSelectors.RootSelector.ElementExists().Invoke(webDriver),
                     div = MainPageSelectors.ByIdSelector("div1").ElementExists().Invoke(webDriver),
                     input;
+
+                if (root != null)
+                {
+                    var nodes = root.FindElements(By.XPath("./li"));
+
+                    foreach(IWebElement node in nodes)
+                    {
+                        var toggler = node.FindElement(By.XPath("./span[contains(@class, 'node-toggler')]"));
+                        if (toggler != null)
+                        {
+                            var nodeNode = node.FindElement(By.XPath("./ul[contains(@class, 'tree-node')]"));
+                        }
+                    }
+
+                    foreach(IWebElement node in root.AsBreadthFirst() /*AsBreadthFirst(root)*/)
+                    {
+                        var text = node.Text;
+                        int index;
+
+                        if ((index = text.IndexOf("\r\n")) != -1)
+                            text = text.Substring(0, index);
+
+                        Debug.WriteLine(text);
+                    }
+                }
 
                 if (div != null)
                 {
@@ -39,6 +68,65 @@ namespace TestSelenium
                     webDriver.Quit();
                     webDriver.Dispose();
                     webDriver = null;
+                }
+            }
+        }
+
+        public static IEnumerable<IWebElement> AsBreadthFirst(IWebElement node)
+        {
+            var q = new Queue<IWebElement>();
+
+            foreach(var childNode in node.FindElements(By.XPath("./li")))
+                q.Enqueue(childNode);
+
+            while (q.Any())
+            {
+                var current = q.Dequeue();
+                var currentText = current.Text;
+                if (current != null)
+                {
+                    try
+                    {
+                        var innerNode = current.FindElement(By.XPath("./ul"));
+                        if (innerNode != null)
+                            foreach (var childNode in innerNode.FindElements(By.XPath("./li")))
+                                q.Enqueue(childNode);
+                    }
+                    catch (NoSuchElementException)
+                    {}
+
+                    yield return current;
+                }
+            }
+        }
+    }
+
+    public static class IWebElementExtensions
+    {
+        public static IEnumerable<IWebElement> AsBreadthFirst(this IWebElement node)
+        {
+            var q = new Queue<IWebElement>();
+
+            foreach (var childNode in node.FindElements(By.XPath("./li")))
+                q.Enqueue(childNode);
+
+            while (q.Any())
+            {
+                var current = q.Dequeue();
+                var currentText = current.Text;
+                if (current != null)
+                {
+                    try
+                    {
+                        var innerNode = current.FindElement(By.XPath("./ul"));
+                        if (innerNode != null)
+                            foreach (var childNode in innerNode.FindElements(By.XPath("./li")))
+                                q.Enqueue(childNode);
+                    }
+                    catch (NoSuchElementException)
+                    { }
+
+                    yield return current;
                 }
             }
         }
