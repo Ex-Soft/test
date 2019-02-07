8c4642f8c74a2f1c2423595fe9c39f17e00d3137
Ex_Soft <4others2@gmail.com>
2019-02-07 17:20:16 +0200

    c#

---
+++ c#/test/DI/CastleWindsor/TestSimple/main.cs
@@ -0,0 +1,33 @@
+﻿// https://www.codementor.io/copperstarconsulting/getting-started-with-dependency-injection-using-castle-windsor-4meqzcsvh
+
+using System;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new WindsorContainer();
+
+            // Register the CompositionRoot type with the container
+            container.Register(Component.For<ICompositionRoot>().ImplementedBy<CompositionRoot>());
+            container.Register(Component.For<IConsoleWriter>().ImplementedBy<ConsoleWriter>());
+
+            // Resolve an object of type ICompositionRoot (ask the container for an instance)
+            // This is analagous to calling new() in a non-IoC application.
+            var root = container.Resolve<ICompositionRoot>();
+
+            root.LogMessage("Hello from my very first resolved class!");
+
+            // Wait for user input so they can check the program's output.
+            Console.ReadLine();
+        }
+    }
+}
