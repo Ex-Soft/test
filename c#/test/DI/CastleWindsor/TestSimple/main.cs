ed87cbd29a35ffaf3a651a559b488b93859e141f
Ex_Soft <4others2@gmail.com>
2019-02-18 15:26:39 +0200

    c#

--- c#/test/DI/CastleWindsor/TestSimple/main.cs
+++ c#/test/DI/CastleWindsor/TestSimple/main.cs
@@ -20,8 +20,17 @@ namespace TestSimple
             //container.Register(Component.For<ISingletonDemo>().ImplementedBy<SingletonDemo>());
             container.Register(Component.For<ISingletonDemo>().ImplementedBy<SingletonDemo>().LifestyleTransient());
 
-            container.Register(Component.For<IClassWithCtorWithParameters>().ImplementedBy<ClassWithCtorWithParameters>().DynamicParameters((kernel, parameters) => { parameters["pInt"] = 13; parameters["pString"] = "SmthString"; }));
-            container.Register(Component.For<IClassWithCtorWithParameters2>().ImplementedBy<ClassWithCtorWithParameters2>().DependsOn(Property.ForKey<int>().Eq(13), Property.ForKey<string>().Eq("SmthString")));
+            container.Register(Component.For<IClassWithCtorWithParameters>().ImplementedBy<ClassWithCtorWithParameters>()
+                .LifestyleTransient()
+                .DynamicParameters((kernel, parameters) =>
+                {
+                    parameters["pInt"] = 13;
+                    parameters["pString"] = "SmthString";
+                }));
+
+            container.Register(Component.For<IClassWithCtorWithParameters2>().ImplementedBy<ClassWithCtorWithParameters2>()
+                .LifestyleTransient()
+                .DependsOn(Property.ForKey<int>().Eq(13), Property.ForKey<string>().Eq("SmthString")));
 
             // Resolve an object of type ICompositionRoot (ask the container for an instance)
             // This is analagous to calling new() in a non-IoC application.
@@ -31,19 +40,23 @@ namespace TestSimple
 
             IClassWithCtorWithParameters
                 classWithCtorWithParameters1 = container.Resolve<IClassWithCtorWithParameters>(),
-                classWithCtorWithParameters2 = container.Resolve<IClassWithCtorWithParameters>(new { pInt = 26, pString = "SmthStringSmthString" });
+                classWithCtorWithParameters2 = container.Resolve<IClassWithCtorWithParameters>(new { pInt = 26, pString = "SmthStringSmthString" }),
+                classWithCtorWithParameters3 = container.Resolve<IClassWithCtorWithParameters>(new Arguments(new { pInt = 39, pString = "SmthStringSmthStringSmthString" }));
 
-            root.LogMessage(classWithCtorWithParameters1.ToString());
-            root.LogMessage(classWithCtorWithParameters2.ToString());
+            Console.WriteLine(new String('-', 60));
+            root.LogMessage(classWithCtorWithParameters1.ToString()); // {pInt:13, pString:"SmthString"}
+            root.LogMessage(classWithCtorWithParameters2.ToString()); // {pInt:13, pString:"SmthString"}
+            root.LogMessage(classWithCtorWithParameters3.ToString()); // {pInt:13, pString:"SmthString"}
 
             IClassWithCtorWithParameters2
                 classWithCtorWithParameters21 = container.Resolve<IClassWithCtorWithParameters2>(),
                 classWithCtorWithParameters22 = container.Resolve<IClassWithCtorWithParameters2>(new { pInt = 26, pString = "SmthStringSmthString" }),
-                classWithCtorWithParameters23 = container.Resolve<IClassWithCtorWithParameters2>(new Arguments(new { pInt = 26, pString = "SmthStringSmthString" }));
+                classWithCtorWithParameters23 = container.Resolve<IClassWithCtorWithParameters2>(new Arguments(new { pInt = 39, pString = "SmthStringSmthStringSmthString" }));
 
-            root.LogMessage(classWithCtorWithParameters21.ToString());
-            root.LogMessage(classWithCtorWithParameters22.ToString());
-            root.LogMessage(classWithCtorWithParameters23.ToString());
+            Console.WriteLine(new string('-', 60));
+            root.LogMessage(classWithCtorWithParameters21.ToString()); // {pInt:13, pString:"SmthString"}
+            root.LogMessage(classWithCtorWithParameters22.ToString()); // {pInt:26, pString:"SmthStringSmthString"}
+            root.LogMessage(classWithCtorWithParameters23.ToString()); // {pInt:39, pString:"SmthStringSmthStringSmthString"}
 
             // Wait for user input so they can check the program's output.
             Console.ReadLine();
