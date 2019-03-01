e216a9663b83d94930f949a434bb85e7b02c9380
Ex_Soft <4others2@gmail.com>
2019-03-01 16:38:33 +0200

    asp.net

---
+++ ASP.NET/test/AngularJS/1.x/component/TestComponent/TestComponent/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Optimization;
+
+namespace TestComponent
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            /*bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));*/
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/angular.js"));
+
+            /*bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));*/
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/app.js"));
+        }
+    }
+}
---
+++ ASP.NET/test/AngularJS/1.x/component/TestComponent/TestComponent/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿/*
+    https://developerhandbook.com/typescript/writing-angularjs-1-x-with-typescript/
+
+    install-package angularjs.core
+    install-package angularjs.TypeScript.DefinitelyTyped
+    update-package jquery.TypeScript.DefinitelyTyped
+    install-package Microsoft.AspNet.Web.Optimization
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace TestComponent
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
