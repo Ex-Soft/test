3ee53979c3765675857148d1db4e3973064ef7d8
Ex_Soft <4others2@gmail.com>
2019-03-05 17:12:53 +0200

    ts

---
+++ TypeScript/test/TestESVersion/TestESVersion/Scripts/TestClassWithInterface.ts
@@ -0,0 +1,7 @@
+﻿class TestClassWithInterface implements ITestInterface {
+    interfaceProperty1: string;
+
+    constructor(aInterfaceProperty1: string) {
+        this.interfaceProperty1 = aInterfaceProperty1;
+    }
+}
