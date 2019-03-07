950ff4db8f2deb220d342f6629b9a5c68123d401
Ex_Soft <4others2@gmail.com>
2019-03-07 16:56:25 +0200

    asp.net

--- ASP.NET/test/AngularJS/1.x/component/TestComponent/TestComponent/App_Start/BundleConfig.cs
+++ ASP.NET/test/AngularJS/1.x/component/TestComponent/TestComponent/App_Start/BundleConfig.cs
@@ -12,8 +12,8 @@ namespace TestComponent
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
                 "~/Scripts/angular.js"));
 
-            /*bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/site.css"));*/
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
                 "~/Scripts/app.js"));
