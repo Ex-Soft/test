624afdadae596adae3dcb0bd36179cfebcd7d9c0
Ex_Soft <4others2@gmail.com>
2019-03-26 10:40:22 +0200

    sql

--- sql/MSSQL/stringArabic.sql
+++ sql/MSSQL/stringArabic.sql
@@ -1,8 +1,8 @@
 ﻿declare
-	@s1 nvarchar(max) = N'هذه الجملة هي رقم 1. هذه الجملة هي رقم 2.',
-	@s2 nvarchar(max) = N'هذا الاقتراح هو رقم 3. هذا الاقتراح هو رقم 4.',
-	@s3 nvarchar(max) = N'هذا الاقتراح رقم 5. هذا الاقتراح رقم 6.',
-	@result1 nvarchar(max) = N'هذا الاقتراح هو 1. هذا هو الاقتراح رقم 2. هذا هو الاقتراح رقم 3. هذا هو الاقتراح رقم 4. هذا هو الاقتراح رقم 5. هذا هو الاقتراح رقم 6.',
+	@s1 nvarchar(max) = N'هذه الجملة 1. هذه الجملة 2.',
+	@s2 nvarchar(max) = N'هذه الجملة 3. هذه الجملة 4.',
+	@s3 nvarchar(max) = N'هذه الجملة 5. هذه الجملة 6.',
+	@result1 nvarchar(max) = N'هذه الجملة 1. هذه الجملة 2. هذه الجملة 3. هذه الجملة 4. هذه الجملة 5. هذه الجملة 6.',
 	@result2 nvarchar(max),
 	@i int = 1,
 	@len1 int,
@@ -19,6 +19,9 @@ print @result2
 set @len1 = len(@result1)
 set @len2 = len(@result2)
 
+print @len1
+print @len2
+
 if @len1 <= @len2
 	set @len = @len1
 else
@@ -28,13 +31,12 @@ while @i <= @len
 	begin
 		set @nchar1 = substring(@result1, @i, 1)
 		set @nchar2 = substring(@result2, @i, 1)
-		print N' '
-		print @i
-		print @nchar1
-		print @nchar2
-		if @nchar1 = @nchar2
-			print N'='
-		else
-			print N'!='
+		if @nchar1 != @nchar2
+			begin
+				print N' '
+				print @i
+				print @nchar1
+				print @nchar2
+			end
 		set @i += 1
 	end
