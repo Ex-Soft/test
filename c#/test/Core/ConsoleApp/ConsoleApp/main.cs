8bd7a94418e2773ae4a8f51a188b3e986c5594b6
Ex_Soft <4others2@gmail.com>
2019-03-26 14:31:45 +0200

    dotnet core

---
+++ c#/test/Core/ConsoleApp/ConsoleApp/main.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
