8d037cdd2ecebc8f1b039491ae5f955d8967ce84
Ex_Soft <4others2@gmail.com>
2019-06-12 17:14:08 +0300

    python

---
+++ python/test/car.py
@@ -0,0 +1,7 @@
+# https://realpython.com/oop-in-python-vs-java/
+
+class Car:
+    def __init__(self, color, model, year):
+        self.color = color
+        self.model = model
+        self.year = year
