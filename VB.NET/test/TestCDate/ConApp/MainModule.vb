2096ddeec2af5f592bc5c9e6ad17c40f02f87e64
Ex_Soft <4others2@gmail.com>
2019-06-21 13:16:27 +0300

    vb.net

---
+++ VB.NET/test/TestCDate/ConApp/MainModule.vb
@@ -0,0 +1,95 @@
+﻿Imports System.Globalization
+Imports System.Runtime.InteropServices
+Imports System.Threading
+
+Module MainModule
+
+    <DllImport("Kernel32.dll", CharSet:=CharSet.Auto)>
+    Public Function GetSystemDefaultUILanguage() As UShort
+
+    End Function
+
+    <DllImport("Kernel32.dll", CharSet:=CharSet.Auto)>
+    Public Function GetUserDefaultUILanguage() As UShort
+
+    End Function
+
+    Sub Main()
+        Console.WriteLine(String.Format("GetSystemDefaultUILanguage(): 0x{0:x4}", GetSystemDefaultUILanguage()))
+        Console.WriteLine(String.Format("GetUserDefaultUILanguage(): 0x{0:x4}", GetUserDefaultUILanguage()))
+        Console.WriteLine()
+
+        Console.WriteLine(String.Format("Thread.CurrentThread.CurrentCulture: ""{0}"" DateTimeFormat: ""{1}""", Thread.CurrentThread.CurrentCulture, Thread.CurrentThread.CurrentCulture.DateTimeFormat.ShortDatePattern))
+        Console.WriteLine(String.Format("Thread.CurrentThread.CurrentUICulture: ""{0}"" DateTimeFormat: ""{1}""", Thread.CurrentThread.CurrentCulture, Thread.CurrentThread.CurrentCulture.DateTimeFormat.ShortDatePattern))
+        Console.WriteLine(String.Format("CultureInfo.CurrentCulture: ""{0}"" DateTimeFormat: ""{1}""", CultureInfo.CurrentCulture, CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern))
+        Console.WriteLine(String.Format("CultureInfo.CurrentUICulture: ""{0}"" DateTimeFormat: ""{1}""", CultureInfo.CurrentUICulture, CultureInfo.CurrentUICulture.DateTimeFormat.ShortDatePattern))
+        Console.WriteLine(String.Format("CultureInfo.DefaultThreadCurrentCulture: ""{0}"" DateTimeFormat: ""{1}""", CultureInfo.DefaultThreadCurrentCulture, If(CultureInfo.DefaultThreadCurrentCulture Is Nothing, "", CultureInfo.DefaultThreadCurrentCulture.DateTimeFormat.ShortDatePattern)))
+        Console.WriteLine(String.Format("CultureInfo.DefaultThreadCurrentUICulture: ""{0}"" DateTimeFormat: ""{1}""", CultureInfo.DefaultThreadCurrentUICulture, If(CultureInfo.DefaultThreadCurrentUICulture Is Nothing, "", CultureInfo.DefaultThreadCurrentUICulture.DateTimeFormat.ShortDatePattern)))
+        Console.WriteLine(String.Format("CultureInfo.InstalledUICulture: ""{0}"" DateTimeFormat: ""{1}""", CultureInfo.InstalledUICulture, CultureInfo.InstalledUICulture.DateTimeFormat.ShortDatePattern))
+        Console.WriteLine(String.Format("CultureInfo.InvariantCulture: ""{0}"" DateTimeFormat: ""{1}""", CultureInfo.InvariantCulture, CultureInfo.InvariantCulture.DateTimeFormat.ShortDatePattern))
+
+        Dim strDDMMYY As String
+        Dim strMMDDYY As String
+        Dim dt As DateTime
+
+        strDDMMYY = "31/12/2019"
+        strMMDDYY = "12/31/2019"
+
+        'strDDMMYY = "08/03/2019"
+        'strMMDDYY = "03/08/2019"
+
+        Try
+            dt = CDate(strDDMMYY)
+            Console.WriteLine(dt.ToLongDateString())
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+
+        Try
+            dt = CDate(strMMDDYY)
+            Console.WriteLine(dt.ToLongDateString())
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+
+        Dim culture As CultureInfo
+
+        'culture = CultureInfo.CreateSpecificCulture("en-US")
+        culture = New CultureInfo("en-US")
+        'culture = New CultureInfo("en-US", False)
+
+        'culture.DateTimeFormat.ShortDatePattern = "M/d/yyyy"
+
+        Thread.CurrentThread.CurrentCulture = culture
+        Thread.CurrentThread.CurrentUICulture = culture
+
+        Console.WriteLine()
+
+        Console.WriteLine(String.Format("Thread.CurrentThread.CurrentCulture: ""{0}"" DateTimeFormat: ""{1}""", Thread.CurrentThread.CurrentCulture, Thread.CurrentThread.CurrentCulture.DateTimeFormat.ShortDatePattern))
+        Console.WriteLine(String.Format("Thread.CurrentThread.CurrentUICulture: ""{0}"" DateTimeFormat: ""{1}""", Thread.CurrentThread.CurrentCulture, Thread.CurrentThread.CurrentCulture.DateTimeFormat.ShortDatePattern))
+        Console.WriteLine(String.Format("CultureInfo.CurrentCulture: ""{0}"" DateTimeFormat: ""{1}""", CultureInfo.CurrentCulture, CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern))
+        Console.WriteLine(String.Format("CultureInfo.CurrentUICulture: ""{0}"" DateTimeFormat: ""{1}""", CultureInfo.CurrentUICulture, CultureInfo.CurrentUICulture.DateTimeFormat.ShortDatePattern))
+        Console.WriteLine(String.Format("CultureInfo.DefaultThreadCurrentCulture: ""{0}"" DateTimeFormat: ""{1}""", CultureInfo.DefaultThreadCurrentCulture, If(CultureInfo.DefaultThreadCurrentCulture Is Nothing, "", CultureInfo.DefaultThreadCurrentCulture.DateTimeFormat.ShortDatePattern)))
+        Console.WriteLine(String.Format("CultureInfo.DefaultThreadCurrentUICulture: ""{0}"" DateTimeFormat: ""{1}""", CultureInfo.DefaultThreadCurrentUICulture, If(CultureInfo.DefaultThreadCurrentUICulture Is Nothing, "", CultureInfo.DefaultThreadCurrentUICulture.DateTimeFormat.ShortDatePattern)))
+        Console.WriteLine(String.Format("CultureInfo.InstalledUICulture: ""{0}"" DateTimeFormat: ""{1}""", CultureInfo.InstalledUICulture, CultureInfo.InstalledUICulture.DateTimeFormat.ShortDatePattern))
+        Console.WriteLine(String.Format("CultureInfo.InvariantCulture: ""{0}"" DateTimeFormat: ""{1}""", CultureInfo.InvariantCulture, CultureInfo.InvariantCulture.DateTimeFormat.ShortDatePattern))
+
+        Try
+            dt = CDate(strDDMMYY)
+            Console.WriteLine(dt.ToLongDateString())
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+
+        Try
+            dt = CDate(strMMDDYY)
+            Console.WriteLine(dt.ToLongDateString())
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ VB.NET/test/TestCDate/ConApp/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConApp")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ConApp")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("bee0f34d-f197-4250-bfd4-313a45f102b5")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
