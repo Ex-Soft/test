d3ba9b8dbf00fb1dfb85ea63fb184db703d7f1fc
Ex_Soft <4others2@gmail.com>
2019-06-27 13:14:17 +0300

    ts

---
+++ TypeScript/test/HelloWorldInTypeScript/HelloWorldInTypeScript/Scripts/ClassWithStatic.ts
@@ -0,0 +1,21 @@
+﻿class ClassWithStatic {
+    static staticP1: string = "staticP1";
+
+    constructor(private privateP1: string) {
+    }
+
+    static WriteToLog(msg: string) {
+        if (window.console && console.log)
+            console.log(msg);
+    }
+
+    smthFunc1() {
+        if (window.console && console.log)
+            console.log("smthFunc1(%o)", arguments);
+    }
+
+    smthFunc2 = function() {
+        if (window.console && console.log)
+            console.log("smthFunc2(%o)", arguments);
+    }
+}
