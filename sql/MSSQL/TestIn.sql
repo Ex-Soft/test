6b3c01ec4d4d06f60f7e9980128cd38380c1cca7
Ex_Soft <4others2@gmail.com>
2019-06-27 13:13:29 +0300

    sql

--- sql/MSSQL/TestIn.sql
+++ sql/MSSQL/TestIn.sql
@@ -5,3 +5,29 @@ insert into @t (id, idMain) values (2, 1)
 insert into @t (id, idMain) values (3, 3)
 
 select * from @t where 1 in (id, idMain)
+
+declare @src table (id int)
+declare @dest table (id int)
+
+insert into @src
+	(id)
+values
+	(1),(3),(5)
+
+insert into @dest
+	(id)
+values
+	(1),(2),(3),(4),(5)
+
+select
+	*
+from
+	@dest
+where
+	id in (select id from @src)
+
+select
+	*
+from
+	@dest dest
+	join @src src on src.id = dest.id
