89bbd862155f5e02453a6f181ef340360d2de4b1
Ex_Soft <4others2@gmail.com>
2019-07-10 13:10:53 +0300

    asp.net mvc

---
+++ ASP.NET/test/mvc/TestPartialView/TestPartialView/Controllers/TestLayout2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestPartialView.Controllers
+{
+    public class TestLayout2Controller : Controller
+    {
+        // GET: TestLayout2
+        public ActionResult Index()
+        {
+            return View("Index", "_TestLayout2");
+        }
+    }
+}
