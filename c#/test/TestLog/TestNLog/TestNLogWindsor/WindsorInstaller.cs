a140f304c9ad2663bc8b252f34cf28030316e22e
Ex_Soft <4others2@gmail.com>
2019-07-11 13:34:41 +0300

    c#

---
+++ c#/test/TestLog/TestNLog/TestNLogWindsor/WindsorInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Castle.Facilities.Logging;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Services.Logging.NLogIntegration;
+using Castle.Windsor;
+
+namespace TestNLogWindsor
+{
+    public class WindsorInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.AddFacility<LoggingFacility>(f => f.LogUsing<NLogFactory>().WithConfig("nlog.config"));
+        }
+    }
+}
