f35dceebcd750ba3a9699f6fa8e45825f9929ce6
Ex_Soft <4others2@gmail.com>
2019-10-23 10:19:27 +0300

    c#

---
+++ c#/test/MongoDB/TestSimple/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
+
+namespace TestSimple
+{
+    public class Student
+    {
+        public ObjectId Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Class { get; set; }
+        public int Age { get; set; }
+        public IEnumerable<string> Subjects { get; set; }
+    }
+}
