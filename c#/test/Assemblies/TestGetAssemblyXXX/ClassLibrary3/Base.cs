bdd9168b14fed82305619318d60b1ff60edd9a2d
Ex_Soft <4others2@gmail.com>
2019-10-25 13:59:48 +0300

    c#

---
+++ c#/test/Assemblies/TestGetAssemblyXXX/ClassLibrary3/Base.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace ClassLibrary3
+{
+    public class Base
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public virtual string GetExecutingAssemblyName()
+        {
+            var assemblyName = System.Reflection.Assembly.GetExecutingAssembly().GetName();
+            var currentMethod = System.Reflection.MethodBase.GetCurrentMethod();
+
+            Debug.WriteLine($"{assemblyName.Name}.Base.{currentMethod.Name}()");
+
+            return assemblyName.Name;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public virtual string GetCallingAssemblyName()
+        {
+            var assemblyName = System.Reflection.Assembly.GetCallingAssembly().GetName();
+            var currentMethod = System.Reflection.MethodBase.GetCurrentMethod();
+
+            Debug.WriteLine($"{assemblyName.Name}.Base.{currentMethod.Name}()");
+
+            return assemblyName.Name;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public virtual string GetEntryAssemblyName()
+        {
+            var assemblyName = System.Reflection.Assembly.GetEntryAssembly().GetName();
+            var currentMethod = System.Reflection.MethodBase.GetCurrentMethod();
+
+            Debug.WriteLine($"{assemblyName.Name}.Base.{currentMethod.Name}()");
+
+            return assemblyName.Name;
+        }
+    }
+}
