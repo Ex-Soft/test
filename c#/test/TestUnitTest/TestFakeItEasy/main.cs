9e1a37eeaef42d75f18be187b4d76248a21360a2
Ex_Soft <4others2@gmail.com>
2019-10-25 14:03:52 +0300

    c#

---
+++ c#/test/TestUnitTest/TestFakeItEasy/main.cs
@@ -0,0 +1,26 @@
+﻿using FakeItEasy;
+
+namespace TestFakeItEasy
+{
+    public interface IMath
+    {
+        int Add(int left, int right);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var math = A.Fake<IMath>();
+            //A.CallTo(() => math.Add(5, 5)).MustHaveHappened();
+            A.CallTo(() => math.Add(1, 2)).Returns(3);
+            A.CallTo(() => math.Add(3, 4)).Returns(7);
+
+            int result;
+
+            result = math.Add(5, 5);
+            result = math.Add(1, 2);
+            result = math.Add(3, 4);
+            result = math.Add(1, 1);
+        }
+    }
+}
