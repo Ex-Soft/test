2dc44cd327e32bfd05d98f4aa936ba9d01f5c54f
Ex_Soft <4others2@gmail.com>
2019-11-06 00:14:06 +0200

    asp.net

---
+++ ASP.NET/test/mvc/TestAuth19/TestAuth19/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TestAuth19
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ASP.NET/test/mvc/TestAuth19/TestAuth19/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestAuth19.Startup))]
+namespace TestAuth19
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
