2b28a9da6d5168f43ee1f203151055fb7a7c23d0
Ex_Soft <4others2@gmail.com>
2019-11-05 23:34:50 +0200

    asp.net

---
+++ ASP.NET/test/mvc/TestAuth17/TestAuth17/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestAuth17.Startup))]
+namespace TestAuth17
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
