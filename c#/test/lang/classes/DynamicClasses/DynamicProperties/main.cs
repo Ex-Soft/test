2fd0e96b23d512c42c787208e6a2cc6faefbbfbc
Ex_Soft <4others2@gmail.com>
2019-11-16 17:17:28 +0200

    c#

---
+++ c#/test/lang/classes/DynamicClasses/DynamicProperties/main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyTypeBuilder.CreateProperty(MyTypeBuilder.GetTypeBuilder(), "DynamicProperty1", typeof(string));
+        }
+    }
+}
