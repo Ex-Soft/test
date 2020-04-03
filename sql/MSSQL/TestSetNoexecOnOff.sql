6e7cce8c38f87b5798884d20679e5a6f099df5df
Ex_Soft <4others2@gmail.com>
2020-04-03 09:14:09 +0300

    sql

--- sql/MSSQL/TestSetNoexecOnOff.sql
+++ sql/MSSQL/TestSetNoexecOnOff.sql
@@ -11,10 +11,23 @@ set noexec off;
 go
 
 alter procedure TestSetNoexecOnOff
+	@input_param1 int,
+	@input_param2 int,
+	@output_param int output
 as
 begin
-	set nocount on
+	set nocount on;
 
-	print N'TestSetNoexecOnOff'
+	print N'TestSetNoexecOnOff';
+
+	set @output_param = @input_param1 + @input_param2;
+
+	return 0;
 end
 go
+
+declare @output int
+exec TestSetNoexecOnOff @input_param1 = 1, @input_param2 = 2, @output_param = @output out
+print @output
+
+--drop procedure TestSetNoexecOnOff
--- sql/MSSQL/like.sql
+++ sql/MSSQL/like.sql
@@ -1,3 +1,22 @@
 SELECT SERVERPROPERTY ('Collation')
-select * from m3m_command where code like '%OU%'
-select * from m3m_command where code like '%OU%' collate Ukrainian_CS_AS
+--select * from m3m_command where code like '%OU%'
+--select * from m3m_command where code like '%OU%' collate Ukrainian_CS_AS
+
+declare @t table ([Value] nvarchar(max))
+
+insert into @t
+values
+	(N'blah-blah-blah'),
+	(N'[text](url)'),
+	(N'[text](url attribute)'),
+	(N'blah-blah-blah [text](url attribute) blah-blah-blah'),
+	(N'[text](url) [text](url)'),
+	(N'[text](url attribute) [text](url attribute)'),
+	(N'blah-blah-blah [text](url attribute) blah-blah-blah [text](url attribute) blah-blah-blah')
+
+select
+	*
+from
+	@t
+where
+	[Value] like N'%](% %)%'
