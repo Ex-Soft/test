986b07e17bf9df8bf45c60c1ce863dee6e440498
Ex_Soft <4others2@gmail.com>
2020-04-29 17:16:14 +0300

    asp.net

---
+++ ASP.NET/test/Core/WebAPI/VS/TestSwagger/TestSwagger/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace TestSwagger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        /// <summary>
+        /// Get values
+        /// </summary>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Get a specific value
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Create a value
+        /// </summary>
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] string value)
+        {
+            return value;
+        }
+
+        /// <summary>
+        /// Update a specific value
+        /// </summary>
+        [HttpPut("{id}")]
+        public ActionResult<string> Put(int id, [FromBody] string value)
+        {
+            return value;
+        }
+
+        /// <summary>
+        /// Delete a specific value
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpDelete("{id}")]
+        public ActionResult<int> Delete(int id)
+        {
+            return id;
+        }
+    }
+}
--- ASP.NET/test/Core/WebAPI/VS/TodoApi/TodoApi/Startup.cs
+++ ASP.NET/test/Core/WebAPI/VS/TodoApi/TodoApi/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -25,11 +26,11 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
