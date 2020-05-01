132ab3985f18eefdb1d39d8b35b72f5aed42687d
Ex_Soft <4others2@gmail.com>
2020-05-01 16:45:01 +0300

    asp.net

--- ASP.NET/test/Core/WebAPI/VSC/WebApp1/Controllers/StaffController.cs
+++ ASP.NET/test/Core/WebAPI/VSC/WebApp1/Controllers/StaffController.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using WebApp1.Models;
 
 namespace WebApp1.Controllers
@@ -18,17 +20,17 @@ namespace WebApp1.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Staff> Get()
+        public async Task<IEnumerable<Staff>> Get()
         {
-            return _context.Staffs.ToArray();
+            return await _context.Staffs.ToListAsync();
         }
 
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<Staff> Get(long id)
+        public async Task<ActionResult<Staff>> Get(long id)
         {
-            var staff = _context.Staffs.FirstOrDefault(e => e.Id == id);
+            var staff = await _context.Staffs.FindAsync(id);
 
             if (staff == null)
                 return NotFound();
@@ -40,17 +42,21 @@ namespace WebApp1.Controllers
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status409Conflict)]
-        public ActionResult<Staff> Post([FromBody] Staff staff)
+        public async Task<ActionResult<Staff>> Post([FromBody] Staff staff)
         {
             if (staff == null)
                 return NotFound();
 
-            if (staff.Id != 0 && _context.Staffs.FirstOrDefault(e => e.Id == staff.Id) != null)
-                return Conflict();
+            if (staff.Id != 0)
+            {
+                var _staff = await _context.Staffs.FindAsync(staff.Id);
+                if (_staff != null)
+                    return Conflict();
+            }
 
             staff.Id = 0;
             _context.Staffs.Add(staff);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return CreatedAtAction(nameof(Get), new { id = staff.Id }, staff);
         }
@@ -58,13 +64,17 @@ namespace WebApp1.Controllers
         [HttpPut("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult Put(long id, [FromBody] Staff staff)
+        public async Task<IActionResult> Put(long id, [FromBody] Staff staff)
         {
             if (id == 0 || staff == null)
                 return NoContent();
             
-            var _staff = _context.Staffs.FirstOrDefault(e => e.Id == id);
+            if (id != staff.Id)
+                return BadRequest();
+
+            var _staff = await _context.Staffs.FindAsync(id);
             if (_staff == null)
                 return NotFound();
             
@@ -73,7 +83,17 @@ namespace WebApp1.Controllers
             _staff.Dep = staff.Dep;
             _staff.BirthDate = staff.BirthDate;
 
-            _context.SaveChanges();
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StaffExists(id))
+                    return NotFound();
+                else
+                    throw;
+            }
 
             return Ok();
         }
@@ -81,16 +101,21 @@ namespace WebApp1.Controllers
         [HttpDelete("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult Delete(long id)
+        public async Task<IActionResult> Delete(long id)
         {
-            var _staff = _context.Staffs.FirstOrDefault(e => e.Id == id);
-            if (_staff == null)
+            var staff = await _context.Staffs.FindAsync(id);
+            if (staff == null)
                 return NotFound();
 
-            _context.Staffs.Remove(_staff);
-            _context.SaveChanges();
+            _context.Staffs.Remove(staff);
+            await _context.SaveChangesAsync();
 
             return Ok();
+        }
+
+        bool StaffExists(long id)
+        {
+            return _context.Staffs.Any(e => e.Id == id);
         }
     }
 }
--- ASP.NET/test/Core/WebAPI/VSC/WebApp1/Program.cs
+++ ASP.NET/test/Core/WebAPI/VSC/WebApp1/Program.cs
@@ -1,3 +1,5 @@
+// https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-3.1&tabs=visual-studio-code
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
