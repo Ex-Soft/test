1da1ac628dae686d1033a691b12c4a8e76508795
Ex_Soft <4others2@gmail.com>
2020-06-19 14:28:34 +0300

    asp.net

--- ASP.NET/test/Core/WebAPI/VS/AnyTest/Controllers/ValuesController.cs
+++ ASP.NET/test/Core/WebAPI/VS/AnyTest/Controllers/ValuesController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
-namespace TodoApi.Controllers
+namespace AnyTest.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -21,25 +19,33 @@ namespace TodoApi.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return $"value {id}";
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Victim value)
         {
+            Debug.WriteLine($"Post: {{value:\"{value.Value}\"}}");
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Victim value)
         {
+            Debug.WriteLine($"Put: {{id:{id}, value:\"{value.Value}\"}}");
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            Debug.WriteLine($"Delete: {{id:{id}}}");
         }
+    }
+
+    public class Victim
+    {
+        public string Value { get; set; }
     }
 }
