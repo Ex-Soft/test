ea660d141a7b5d5516603211a358675589624761
Ex_Soft <4others2@gmail.com>
2020-06-23 14:44:45 +0300

    c#

---
+++ c#/test/Core/TestTemplates/1.ps1
@@ -0,0 +1,6 @@
+dotnet new -i .\TestTemplate
+dotnet new consolefromtemplate -h
+dotnet new consolefromtemplate -o ConsoleFromTemplate
+dotnet new consolefromtemplate -o ConsoleFromTemplate --name User
+dotnet new consolefromtemplate -o ConsoleFromTemplate --name User --EnableType
+dotnet new -u FullPathToTestTemplate
