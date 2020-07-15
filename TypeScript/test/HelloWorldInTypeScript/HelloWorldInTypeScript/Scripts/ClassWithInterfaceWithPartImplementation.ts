b5ea9f1e67f107885d91a93191554c30a08d7893
Ex_Soft <4others2@gmail.com>
2020-07-15 15:01:55 +0300

    ts

---
+++ TypeScript/test/HelloWorldInTypeScript/HelloWorldInTypeScript/Scripts/ClassWithInterfaceWithPartImplementation.ts
@@ -0,0 +1,10 @@
+﻿class ClassWithInterfaceWithPartImplementation implements IInterfaceWithPartImplementation {
+    pString2: string;
+    pString3: string;
+
+    constructor(public pString1: string, pString4?: string) {
+        this.pString2 = ".ctor()";
+    }
+}
+
+let classWithInterfaceWithPartImplementation = new ClassWithInterfaceWithPartImplementation("RunTime");
