13af59f0bdce358ed888e94697a4abc471f4ddb9
Ex_Soft <4others2@gmail.com>
2020-08-07 12:24:03 +0300

    k8s

--- k8s/local.ps1
+++ k8s/local.ps1
@@ -12,6 +12,18 @@ kubectl get componentstatuses
 kubectl cluster-info
 kubectl get nodes
 
+kubectl create deployment kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1
+kubectl get deployments
+
+kubectl proxy
+curl http://localhost:8001/version
+
+kubectl get pods -o go-template --template '{{range .items}}{{.meadata.name}}{{"\n"}}{{end}}'
+
+kubectl describe pods
+
+curl http://localhost:8001/api/v1/namespaces/default/pods/kubernetes-bootcamp-6f6656d949-cnmbt/proxy/
+
 minikube stop
 minikube delete
 
