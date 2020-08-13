e5f871481ab7c183123eceb4ba41efba3af9c064
Ex_Soft <4others2@gmail.com>
2020-08-13 09:52:03 +0300

    Jsonnet

--- docker/TestJsonnet/1.ps1
+++
@@ -1,9 +0,0 @@
-docker build --tag testworkdir .
-docker run -it --rm testworkdir
-docker ps -a
-docker ps -aq
-docker stop $(docker ps -aq)
-docker images
-docker images -q
-docker rmi $(docker images -q)
-docker rmi testworkdir
