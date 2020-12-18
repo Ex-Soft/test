27abc302c39990b6d5caf57e403d7e0532562877
Ex_Soft <4others2@gmail.com>
2020-12-18 12:36:51 +0200

    ts

--- TypeScript/test/HelloWorldInTypeScript/HelloWorldInTypeScript/Scripts/EnumVariables.ts
+++ TypeScript/test/HelloWorldInTypeScript/HelloWorldInTypeScript/Scripts/EnumVariables.ts
@@ -17,3 +17,16 @@ summerName = Season[Season.Summer];
 
 let season: Season;
 season = Season["Fall"];
+
+enum Direction {
+    Up = "Up",
+    Down = "Down",
+    Left = "Left",
+    Right = "Right"
+}
+
+let directionStr1 = Direction.Right;
+let directionStr2 = Direction[Direction.Right];
+if (window.console && console.log) {
+    console.log("directionStr1=\"%s\" directionStr2=\"%s\"", directionStr1, directionStr2);
+}
