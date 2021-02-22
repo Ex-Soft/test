6238c65e8a4ec3f4c7e98db4353a4e5d454d3bf7
Ex_Soft <4others2@gmail.com>
2021-02-22 11:17:58 +0200

    asp.net

--- ASP.NET/test/angular/angular.io/TestHttp/test-http/src/app/components/test-async/test-async.component.ts
+++ ASP.NET/test/angular/angular.io/TestHttp/test-http/src/app/components/test-async/test-async.component.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { IItemDto } from 'src/app/core/state/item';
+import { TestStoreQuery, TestStoreService } from 'src/app/core/state/test-store';
 
 @Component({
   selector: 'app-test-async',
@@ -14,13 +14,28 @@ export class TestAsyncComponent implements OnInit {
   itemsAsync: Observable<IItemDto[]>;
   itemsSubscribe: IItemDto[];
   itemsBehaviorSubject = new BehaviorSubject<IItemDto[]>([]);
+  activeItem$: Observable<IItemDto>;
+  activeItem: IItemDto;
   baseUrl = 'http://localhost:53683/api/item';
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private testStoreService: TestStoreService,
+    private testStoreQuery: TestStoreQuery
   ) {}
 
   ngOnInit(): void {
+    this.testStoreQuery.items$.subscribe(state => {
+      if (!state.loading && !state.error && Array.isArray(state.ids) && state.ids.length && !state.active) {
+        if (!this.testStoreQuery.hasActive(state.ids[0])) {
+          this.testStoreQuery.__store__.setActive(state.ids[0]);
+          this.activeItem$ = this.testStoreQuery.selectActive();
+          this.activeItem$.subscribe(item => {
+            this.activeItem = item;
+          });
+        }
+      }
+    });
   }
 
   onLoadAsyncAndSubscribeTogetherClick(): void {
@@ -40,5 +55,9 @@ export class TestAsyncComponent implements OnInit {
   onLoadBehaviorSubjectClick(): void {
     this.httpClient.get<IItemDto[]>(this.baseUrl)
       .subscribe(items => this.itemsBehaviorSubject.next(items));
+  }
+
+  onLoadAndSetActiveClick(): void {
+    this.testStoreService.getItems();
   }
 }
