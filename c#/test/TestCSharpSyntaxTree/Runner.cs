0e2374525d002a8edd8bf42d75e054b5f1d5cbc1
Ex_Soft <4others2@gmail.com>
2021-04-19 12:00:55 +0300

    TestCSharpSyntaxTree

---
+++ c#/test/TestCSharpSyntaxTree/Runner.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+namespace TestCSharpSyntaxTree
+{
+    public static class Runner
+    {
+        public static void Run(IVictim victim)
+        {
+            switch (victim)
+            {
+                case Victim1 victim1:
+                {
+                    victim1.Foo1();
+                    break;
+                }
+                case Victim2 victim2:
+                {
+                    victim2.Foo1();
+                    break;
+                }
+                default:
+                {
+                    WriteLine($"Unknown type: \"{victim.GetType().Name}\"");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ c#/test/TestCSharpSyntaxTree/Victim.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using static System.Console;
+
+namespace TestCSharpSyntaxTree
+{
+    public interface IVictim
+    {
+        public void Foo1();
+    }
+
+    public class Victim1 : IVictim
+    {
+        public void Foo1()
+        {
+            WriteLine($"{GetType().Name}.{MethodBase.GetCurrentMethod().Name}()");
+        }
+    }
+    public class Victim2 : IVictim
+    {
+        public void Foo1()
+        {
+            WriteLine($"{GetType().Name}.{MethodBase.GetCurrentMethod().Name}()");
+        }
+    }
+
+    public class Victim3 : IVictim
+    {
+        public void Foo1()
+        {
+            WriteLine($"{GetType().Name}.{MethodBase.GetCurrentMethod().Name}()");
+        }
+    }
+}
