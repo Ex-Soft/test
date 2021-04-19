be14d1ee44ae3534c9717261e1971a677ce07601
Ex_Soft <4others2@gmail.com>
2021-04-19 13:06:55 +0300

    docker

--- docker/1.ps1
+++ docker/1.ps1
@@ -1,3 +1,11 @@
+docker volume create my-vol
+docker volume ls
+docker volume inspect my-vol
+docker volume rm my-vol
+docker run -it --rm -v ${pwd}:/mnt --name testworkdir testworkdir
+
+docker exec -it confluent /bin/bash
+
 docker ps -a
 docker ps -aq
 docker stop $(docker ps -aq)
--- docker/TestWorkdir/1.ps1
+++ docker/TestWorkdir/1.ps1
@@ -1,9 +1,22 @@
-docker build --tag testworkdir .
-docker run -it --rm testworkdir
-docker ps -a
-docker ps -aq
-docker stop $(docker ps -aq)
 docker images
+docker images --no-trunc
+docker images testworkdir
+docker images testworkdir:latest
 docker images -q
+docker images -q testworkdir
+docker images -q testworkdir:latest
+
 docker rmi $(docker images -q)
+docker rmi $(docker images -q testworkdir)
 docker rmi testworkdir
+
+docker build --tag testworkdir .
+docker run -it --rm -v ${pwd}:/mnt --name testworkdir testworkdir
+
+docker ps -a
+docker ps -a -f "name=testworkdir"
+docker ps -aq
+docker ps -aq -f "name=testworkdir"
+
+docker stop $(docker ps -aq)
+docker stop $(docker ps -aq -f "name=testworkdir")
