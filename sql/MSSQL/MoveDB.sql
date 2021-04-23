96bccf744df614887673fdd473f16720c167da5b
Ex_Soft <4others2@gmail.com>
2021-04-23 11:10:04 +0300

    sql

--- sql/MSSQL/MoveDB.sql
+++ sql/MSSQL/MoveDB.sql
@@ -1,16 +1,16 @@
---alter database [TestPlanogram] set offline;
+--alter database testdb set offline;
 
 --select * from sys.databases
 --select * from sys.database_files
 --select * from sys.master_files
-select * from sys.master_files where database_id = db_id(N'TestPlanogram') order by name
+select * from sys.master_files where database_id = db_id(N'testdb') order by name
 
 /*
-alter database [TestPlanogram] modify file (name = [TestPlanogram], filename = N'd:\db\TestPlanogram.mdf');
-alter database [TestPlanogram] modify file (name = [TestPlanogram_log], filename = N'd:\db\TestPlanogram_log.ldf');
+alter database testdb modify file (name = testdb, filename = N'c:\db\mssql\testdb.mdf');
+alter database testdb modify file (name = testdb_log, filename = N'c:\db\mssql\testdb_log.ldf');
 */
 
---alter database [TestPlanogram] set online;
+--alter database testdb set online;
 
 --NT SERVICE\MSSQLSERVER
 --NETWORK SERVICE
