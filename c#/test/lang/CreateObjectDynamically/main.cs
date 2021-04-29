e83abaee0fc1fae942533c721137bb922a97bc67
Ex_Soft <4others2@gmail.com>
2021-04-29 09:22:50 +0300

    c#

--- c#/test/lang/CreateObjectDynamically/main.cs
+++ c#/test/lang/CreateObjectDynamically/main.cs
@@ -39,8 +39,8 @@ namespace CreateObjectDynamically
         static void Main(string[] args)
         {
             #if CREATE_GENERIC
-                GenericCreator.CreateGeneric(typeof(A));
-                GenericCreator.CreateGeneric<B>();
+                CreateGeneric(typeof(A));
+                CreateGeneric<B>();
             #endif
 
             #if CREATE_BY_CLSID
@@ -50,6 +50,42 @@ namespace CreateObjectDynamically
             #if CREATE_BY_TYPE
                 CreateByType();
             #endif
+        }
+        public static void CreateGeneric(Type type)
+        {
+            var methods = typeof(Program).GetRuntimeMethods();
+            var methodInfo = typeof(Program).GetMethod("CreateGeneric", Array.Empty<Type>());
+            var methodInfoConstructed = methodInfo?.MakeGenericMethod(type);
+            methodInfoConstructed?.Invoke(null, Array.Empty<object>());
+        }
+
+        public static void CreateGeneric<T>()
+        {
+            var dataType = new[] { typeof(T) };
+            var genericBase = typeof(GenericClass<>);
+            var combinedType = genericBase.MakeGenericType(dataType);
+
+            object genericInstance = null;
+
+            try
+            {
+                genericInstance = Activator.CreateInstance(combinedType, "1st", "2nd");
+            }
+            catch (MissingMethodException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            }
+
+            var methods = genericInstance?.GetType().GetRuntimeMethods();
+
+            var method = genericInstance?.GetType().GetRuntimeMethod("Foo", new[] { typeof(string) });
+
+            var result = method?.Invoke(genericInstance, new object?[] { "arg" });
+
+            if (genericInstance is IGenericClass<T> interfaceObject)
+            {
+                T result2 = interfaceObject.Foo("arg");
+            }
         }
 
         static void CreateByCLSID()
