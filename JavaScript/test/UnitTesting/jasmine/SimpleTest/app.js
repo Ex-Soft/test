0b0c0485b0dc5a4e40e58cd627dca83274aec659
Ex_Soft <4others2@gmail.com>
2021-09-06 10:18:58 +0300

    js

--- JavaScript/test/UnitTesting/jasmine/SimpleTest/app.js
+++ JavaScript/test/UnitTesting/jasmine/SimpleTest/app.js
@@ -5,3 +5,30 @@
 function subtract(num1, num2) {
 	return num1 - num2;
 }
+
+function multiply(num1, num2) {
+	return num1 * num2;
+}
+
+function justVoidFunction() {
+	if (window.console && console.log) {
+		console.log("justVoidFunction()");
+	}
+}
+
+class SmthClass {
+	smthMethod(param) {
+		const msg = `smthMethod(${param.toString()})`;
+		if (window.console && console.log) {
+			console.log(msg);
+		}
+		return msg;
+	}
+
+	callerFn() {
+		return this.smthMethod("from callerFn()");
+	}
+}
+
+let smthClass = new SmthClass();
+smthClass.smthMethod("app");
--- JavaScript/test/UnitTesting/jasmine/SimpleTest/appspecs.js
+++ JavaScript/test/UnitTesting/jasmine/SimpleTest/appspecs.js
@@ -1,4 +1,7 @@
-﻿describe("Addition", function () {
+﻿// https://www.htmlgoodies.com/javascript/spy-on-javascript-methods-using-the-jasmine-testing-framework/
+// https://volaresystems.com/technical-posts/mocking-calls-with-jasmine
+
+describe("Addition", function () {
 	it("test add() is defined", function () {
 		expect(add).toBeDefined();
 	});
@@ -56,8 +59,6 @@ describe("A spy", function() {
 	});
 });
 
-
-
 describe("A spy, when configured to call through", function() {
 	var
 		foo,
@@ -90,5 +91,73 @@ describe("A spy, when configured to call through", function() {
 
 	it("when called returns the requested value", function() {
 		expect(fetchedBar).toEqual(123);
+	});
+});
+
+describe("spyOn", () => {
+	let testSmthClass;
+	beforeEach(() => { testSmthClass = new SmthClass() });
+	afterEach(() => { testSmthClass = undefined });
+
+	it("test call", () => {
+		spyOn(testSmthClass, "smthMethod");
+		testSmthClass.callerFn();
+		expect(testSmthClass.smthMethod).toHaveBeenCalled();
+	});
+});
+
+describe("createSpyObj", () => {
+	let testSmthClass;
+	beforeEach(() => { testSmthClass = new SmthClass() });
+	afterEach(() => { testSmthClass = undefined });
+
+	it("test call# 1", () => {
+		testSmthClass.smthMethod = jasmine.createSpy("smthMethod spy");
+		testSmthClass.callerFn();
+		expect(testSmthClass.smthMethod).toHaveBeenCalled();
+	});
+
+	it("test call# 2", () => {
+		testSmthClass.smthMethod = jasmine.createSpy("smthMethod() spy");
+		testSmthClass.callerFn();
+		expect(testSmthClass.smthMethod).toHaveBeenCalled();
+	});
+
+	it("test call# 3", () => {
+		testSmthClass.smthMethod = jasmine.createSpy("smthMethod() spy").and.returnValue("test call# 3");
+		testSmthClass.callerFn();
+		expect(testSmthClass.smthMethod).toHaveBeenCalled();
+	});
+
+	it("test call# 4", () => {
+		testSmthClass.smthMethod = jasmine.createSpy("smthMethod() spy").and.callFake(() => {
+			if (window.console && console.log) {
+				console.log("test call# 4");
+			}
+			return "test call# 4";
+		});
+		testSmthClass.callerFn();
+		expect(testSmthClass.smthMethod).toHaveBeenCalled();
+	});
+});
+
+describe("createSpyObj", () => {
+	it("test call", () => {
+		const spy = jasmine.createSpyObj("SmthClass", ["smthMethod"]);
+		spy.smthMethod.and.returnValue("spy");
+		const actual = spy.smthMethod("it");
+		expect(actual).toBe("spy");
+	});
+
+	it("test call fake", () => {
+		const spy = jasmine.createSpyObj("SmthClass", ["smthMethod"]);
+		spy.smthMethod.and.callFake(() => {
+			if (window.console && console.log) {
+				console.log("callFake");
+			}
+			return "spy";
+		});
+		const actual = spy.smthMethod("it");
+		expect(actual).toBe("spy");
 	});
 });
