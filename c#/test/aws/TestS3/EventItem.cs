e5cc78694546710c97a7eb10854fee8d2f6b4660
Ex_Soft <4others2@gmail.com>
2022-01-21 16:49:08 +0200

    c#

---
+++ c#/test/aws/TestS3/EventItem.cs
@@ -0,0 +1,22 @@
+﻿using AthenaNetCore.BusinessLogic.Entities;
+
+namespace TestS3
+{
+    public class EventItem
+    {
+        [AthenaColumn("id")]
+        public string Id { get; set; }
+
+        [AthenaColumn("groupId")]
+        public int GroupId { get; set; }
+
+        [AthenaColumn("value")]
+        public string Value { get; set; }
+
+        [AthenaColumn("fDateTime")]
+        public DateTime FDateTime { get; set; }
+
+        [AthenaColumn("fTimeStamp")]
+        public DateTime FTimeStamp { get; set; }
+    }
+}
