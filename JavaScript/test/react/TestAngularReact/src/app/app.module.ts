0a4c3aba6d30541f24a2820d5db8fdff9abb270d
Ex_Soft <4others2@gmail.com>
2022-02-17 20:38:35 +0200

    js

--- JavaScript/test/react/TestAngularReact/src/app/app.module.ts
+++ JavaScript/test/react/TestAngularReact/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ReactComponentWrapperComponent } from './components/react-component-wrapper/react-component-wrapper.component';
