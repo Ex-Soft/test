ead68e824850c4f135f178bd245b32fae9888de1
Ex_Soft <4others2@gmail.com>
2022-03-16 09:33:59 +0200

    js

--- JavaScript/test/UnitTesting/jasmine/SimpleTest/app.js
+++ JavaScript/test/UnitTesting/jasmine/SimpleTest/app.js
@@ -10,6 +10,10 @@ function multiply(num1, num2) {
 	return num1 * num2;
 }
 
+function addDays(date, value) {
+	return new Date(date.getTime() + value * 86400000);
+}
+
 function justVoidFunction() {
 	if (window.console && console.log) {
 		console.log("justVoidFunction()");
--- JavaScript/test/UnitTesting/jasmine/SimpleTest/appspecs.js
+++ JavaScript/test/UnitTesting/jasmine/SimpleTest/appspecs.js
@@ -108,6 +108,50 @@ describe("Multiplication", function () {
 	});
 });
 
+describe("Date", function () {
+	it("test addDays()", function () {
+		let date = new Date(2020, 1, 28);
+        let actual = addDays(date, -1);
+        expect(actual.getFullYear()).toBe(2020);
+        expect(actual.getMonth()).toBe(1);
+        expect(actual.getDate()).toBe(27);
+		expect(actual.getHours()).toBe(0);
+        expect(actual.getMinutes()).toBe(0);
+        expect(actual.getSeconds()).toBe(0);
+        expect(actual.getMilliseconds()).toBe(0);
+		
+		date = new Date(2020, 1, 28);
+        actual = addDays(date, 1);
+        expect(actual.getFullYear()).toBe(2020);
+        expect(actual.getMonth()).toBe(1);
+        expect(actual.getDate()).toBe(29);
+		expect(actual.getHours()).toBe(0);
+        expect(actual.getMinutes()).toBe(0);
+        expect(actual.getSeconds()).toBe(0);
+        expect(actual.getMilliseconds()).toBe(0);
+
+        date = new Date(2020, 1, 29);
+        actual = addDays(date, 1);
+        expect(actual.getFullYear()).toBe(2020);
+        expect(actual.getMonth()).toBe(2);
+        expect(actual.getDate()).toBe(1);
+		expect(actual.getHours()).toBe(0);
+        expect(actual.getMinutes()).toBe(0);
+        expect(actual.getSeconds()).toBe(0);
+        expect(actual.getMilliseconds()).toBe(0);
+		
+		date = new Date(2020, 11, 31);
+        actual = addDays(date, 1);
+        expect(actual.getFullYear()).toBe(2021);
+        expect(actual.getMonth()).toBe(0);
+        expect(actual.getDate()).toBe(1);
+		expect(actual.getHours()).toBe(0);
+        expect(actual.getMinutes()).toBe(0);
+        expect(actual.getSeconds()).toBe(0);
+        expect(actual.getMilliseconds()).toBe(0);
+	});
+});
+
 describe("A spy", function() {
 	var
 		foo,
