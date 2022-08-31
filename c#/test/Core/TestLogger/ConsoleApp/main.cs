39acb237baee25eeaf5377db515bf94406e3a71f
Ex_Soft <4others2@gmail.com>
2022-08-31 20:50:51 +0300

    c#

---
+++ c#/test/Core/TestLogger/ConsoleApp/main.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder
+        .AddFilter("Microsoft", LogLevel.Warning)
+        .AddFilter("System", LogLevel.Warning)
+        .AddFilter("NonHostConsoleApp.Program", LogLevel.Debug)
+        .AddConsole();
+});
+
+ILogger logger = loggerFactory.CreateLogger<Program>();
+logger.LogInformation("Info Log");
+logger.LogWarning("Warning Log");
+logger.LogError("Error Log");
+logger.LogCritical("Critical Log");
+Console.ReadLine();
---
+++ c#/test/Core/TestLogger/ConsoleAppGenericHost/Worker.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ConsoleAppGenericHost;
+
+public class Worker
+{
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    public void Execute()
+    {
+        _logger.LogInformation("Info Log");
+        _logger.LogWarning("Warning Log");
+        _logger.LogError("Error Log");
+        _logger.LogCritical("Critical Log");
+    }
+}
