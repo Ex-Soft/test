09b5cbfe418c4cbccb7bba29cae70a9c83c6ad2c
Ex_Soft <4others2@gmail.com>
2022-09-30 15:47:38 +0300

    js

--- JavaScript/test/angular/angular.io/components/test-primeng/src/app/components/test-dropdown/test-dropdown.component.ts
+++ JavaScript/test/angular/angular.io/components/test-primeng/src/app/components/test-dropdown/test-dropdown.component.ts
@@ -6,15 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./test-dropdown.component.css']
 })
 export class TestDropdownComponent {
-  options = [
+  options1 = [
     { label: "1st", value: 1 },
     { label: "2nd", value: 2 },
     { label: "3rd", value: 3 }
   ];
 
-  selectedOption = 2;
+  options2 = [
+    { name: "1st", id: 1 },
+    { name: "2nd", id: 2 },
+    { name: "3rd", id: 3 }
+  ];
+
+  selectedOption1 = 2;
+  selectedOption2 = 3;
 
   onChange(e: Event) {
-    console.log(e, this.selectedOption);
+    console.log(e);
   }
 }
