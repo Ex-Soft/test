d2bfb186b475cd2a855c56809f91218dcc6b870c
Ex_Soft <4others2@gmail.com>
2023-03-31 17:16:09 +0300

    c#

---
+++ c#/test/azure/ServiceBus/Consumer/main.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Messaging.ServiceBus;
+
+ServiceBusClient client;
+ServiceBusProcessor processor;
+
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body} from subscription.");
+
+    // complete the message. messages is deleted from the subscription. 
+    //await args.CompleteMessageAsync(args.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
+
+client = new ServiceBusClient("Endpoint=sb://.servicebus.windows.net/;SharedAccessKeyName=;SharedAccessKey=;EntityPath=importevent");
+processor = client.CreateProcessor("importevent", "importevent", new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("Wait for a minute and then press any key to end the processing");
+    Console.ReadKey();
+
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
