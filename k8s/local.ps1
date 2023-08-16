eb58b75ce638b19bc1194df54f0386f75e7a8fbf
Ex_Soft <4others2@gmail.com>
2023-08-16 10:24:29 +0300

    k8s

--- k8s/local.ps1
+++ k8s/local.ps1
@@ -56,3 +56,14 @@ eksctl create cluster --name MYSUPERCLUSTER
 kubectl rollout restart deployment
 kubectl scale deployment purchasehistory-staging --replicas=0
 kubectl scale deployment purchasehistory-staging --replicas=2
+
+az login
+az account set --subscription [subscription-id]
+az aks get-credentials --resource-group [resource-group-name] --name [aks-cluster-name] --admin
+az aks command invoke --resource-group [resource-group-name] --name [aks-cluster-name] --command "kubectl get pods --all-namespaces"
+az aks command invoke --resource-group [resource-group-name] --name [aks-cluster-name] --command "kubectl get pods --namespace=production"
+az aks command invoke --resource-group [resource-group-name] --name [aks-cluster-name] --command "kubectl rollout restart deployment keycloak --namespace=production"
+az aks command invoke --resource-group [resource-group-name] --name [aks-cluster-name] --command "kubectl exec --stdin --tty keycloak-b64f778d7-m78hs --namespace=production -- /bin/bash"
+az aks command invoke --resource-group [resource-group-name] --name [aks-cluster-name] --command "kubectl exec keycloak-7cfcfc6494-28zlc --namespace=production -- env"
+az aks command invoke --resource-group [resource-group-name] --name [aks-cluster-name] --command "kubectl exec keycloak-7cfcfc6494-28zlc --namespace=production -- ls -lrta /opt/jboss/keycloak/standalone/log"
+az aks command invoke --resource-group [resource-group-name] --name [aks-cluster-name] --command "kubectl cp production/keycloak-7cfcfc6494-28zlc:/opt/jboss/keycloak/standalone/log/server.log keycloakserver.log" --file .
