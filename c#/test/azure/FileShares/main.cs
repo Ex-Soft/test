21003b72e81e6499ed69447b77325092be33ab9d
Ex_Soft <4others2@gmail.com>
2024-04-19 17:14:42 +0300

    c#

---
+++ c#/test/azure/FileShares/main.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Storage.Files.Shares;
+using Azure.Storage.Files.Shares.Models;
+
+// az storage account show-connection-string --name StorageAccount --resource-group ResourceGroup
+
+string connectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=;AccountKey=;FileEndpoint=https://.file.core.windows.net/";
+string shareName = "";
+
+ShareClient share = new ShareClient(connectionString, shareName);
+
+var remaining = new Queue<ShareDirectoryClient>();
+remaining.Enqueue(share.GetRootDirectoryClient());
+while (remaining.Count > 0)
+{
+    // Get all of the next directory's files and subdirectories
+    ShareDirectoryClient dir = remaining.Dequeue();
+    foreach (ShareFileItem item in dir.GetFilesAndDirectories())
+    {
+        // Print the name of the item
+        Console.WriteLine(item.Name);
+
+        // Keep walking down directories
+        if (item.IsDirectory)
+        {
+            remaining.Enqueue(dir.GetSubdirectoryClient(item.Name));
+        }
+        else
+        {
+            Console.WriteLine(item.Name);
+        }
+    }
+}
---
+++ c#/test/azure/ServiceBus/Receiver/main.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Messaging.ServiceBus;
+
+using static System.Console;
+
+const string connectionString = "Endpoint=sb://.servicebus.windows.net/;SharedAccessKeyName=;SharedAccessKey=;EntityPath=importdealeradmin";
+const string topicName = "importdealeradmin";
+const string subscriptionName = "importdealeradmin";
+
+ServiceBusClient client = new ServiceBusClient(connectionString);
+
+ServiceBusReceiverOptions receiverOptions = new ServiceBusReceiverOptions { SubQueue = SubQueue.DeadLetter };
+ServiceBusReceiver receiver = client.CreateReceiver(topicName, subscriptionName, receiverOptions);
+
+var message = await receiver.PeekMessageAsync();
+
+if (message?.Body != null)
+{
+    WriteLine($"Body: {message.Body}");
+}
