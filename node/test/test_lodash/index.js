84a93980462ee0392c560ec74f036e1d3784ca2d
Ex_Soft <4others2@gmail.com>
2024-04-25 09:58:05 +0300

    node

--- node/test/test_lodash/index.js
+++ node/test/test_lodash/index.js
@@ -33,9 +33,9 @@ console.log("prototype.fullName = \"%s\" (typeof prototype.fullName = \"%s\")", 
 const personByPrototypeCloneByObjectCreate = Object.create(personByPrototype);
 console.log("objectCreate.fullName = \"%s\" (typeof objectCreate.fullName = \"%s\")", personByPrototypeCloneByObjectCreate.fullName(), typeof personByPrototypeCloneByObjectCreate.fullName);
 const personByPrototypeCloneByStructuredClone = structuredClone(personByPrototype);
-console.log("typeof structuredClone.fullName = \"%s\"", typeof personByPrototypeCloneByStructuredClone.fullName);
+console.log("typeof structuredClone.fullName = \"%s\"", typeof personByPrototypeCloneByStructuredClone.fullName); // undefined
 const personByPrototypeCloneByClone = {...personByPrototype};
-console.log("typeof clone.fullName = \"%s\"", typeof personByPrototypeCloneByClone.fullName);
+console.log("typeof clone.fullName = \"%s\"", typeof personByPrototypeCloneByClone.fullName);  // undefined
 const personByPrototypeCloneByLodash = _.cloneDeep(personByPrototype);
 console.log("_.cloneDeep.fullName = \"%s\" (typeof _.cloneDeep.fullName = \"%s\")", personByPrototypeCloneByLodash.fullName(), typeof personByPrototypeCloneByLodash.fullName);
 
@@ -44,8 +44,8 @@ console.log("class.fullName = \"%s\" (typeof class.fullName = \"%s\")", personBy
 const personByClassCloneByObjectCreate = Object.create(personByClass);
 console.log("objectCreate.fullName = \"%s\" (typeof objectCreate.fullName = \"%s\")", personByClassCloneByObjectCreate.fullName, typeof personByClassCloneByObjectCreate.fullName);
 const personByClassCloneByStructuredClone = structuredClone(personByClass);
-console.log("typeof structuredClone.fullName = \"%s\"", typeof personByClassCloneByStructuredClone.fullName);
+console.log("typeof structuredClone.fullName = \"%s\"", typeof personByClassCloneByStructuredClone.fullName); // undefined
 const personByClassCloneByClone = {...personByClass};
-console.log("typeof clone.fullName = \"%s\"", typeof personByClassCloneByClone.fullName);
+console.log("typeof clone.fullName = \"%s\"", typeof personByClassCloneByClone.fullName); // undefined
 const personByClassCloneByLodash = _.cloneDeep(personByClass);
 console.log("_.cloneDeep.fullName = \"%s\" (typeof _.cloneDeep.fullName = \"%s\")", personByClassCloneByLodash.fullName, typeof personByClassCloneByLodash.fullName);
