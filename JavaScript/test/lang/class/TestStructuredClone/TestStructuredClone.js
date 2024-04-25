a27d9d1558009d62451176784433783a9c959ffe
Ex_Soft <4others2@gmail.com>
2024-04-25 09:58:16 +0300

    js

--- JavaScript/test/lang/class/TestStructuredClone/TestStructuredClone.js
+++ JavaScript/test/lang/class/TestStructuredClone/TestStructuredClone.js
@@ -28,17 +28,17 @@ function onLoad() {
 		const personByPrototypeCloneByObjectCreate = Object.create(personByPrototype);
 		console.log("objectCreate.fullName = \"%s\" (typeof objectCreate.fullName = \"%s\")", personByPrototypeCloneByObjectCreate.fullName(), typeof personByPrototypeCloneByObjectCreate.fullName);
 		const personByPrototypeCloneByStructuredClone = structuredClone(personByPrototype);
-		console.log("typeof structuredClone.fullName = \"%s\"", typeof personByPrototypeCloneByStructuredClone.fullName);
+		console.log("typeof structuredClone.fullName = \"%s\"", typeof personByPrototypeCloneByStructuredClone.fullName); // undefined
 		const personByPrototypeCloneByClone = {...personByPrototype};
-		console.log("typeof clone.fullName = \"%s\"", typeof personByPrototypeCloneByClone.fullName);
+		console.log("typeof clone.fullName = \"%s\"", typeof personByPrototypeCloneByClone.fullName); // undefined
 		
 		const personByClass = new PersonByClass("PersonFirstNameByClass", "PersonLastNameByClass");
 		console.log("class.fullName = \"%s\" (typeof class.fullName = \"%s\")", personByClass.fullName, typeof personByClass.fullName);
 		const personByClassCloneByObjectCreate = Object.create(personByClass);
 		console.log("objectCreate.fullName = \"%s\" (typeof objectCreate.fullName = \"%s\")", personByClassCloneByObjectCreate.fullName, typeof personByClassCloneByObjectCreate.fullName);
 		const personByClassCloneByStructuredClone = structuredClone(personByClass);
-		console.log("typeof structuredClone.fullName = \"%s\"", typeof personByClassCloneByStructuredClone.fullName);
+		console.log("typeof structuredClone.fullName = \"%s\"", typeof personByClassCloneByStructuredClone.fullName); // undefined
 		const personByClassCloneByClone = {...personByClass};
-		console.log("typeof clone.fullName = \"%s\"", typeof personByClassCloneByClone.fullName);
+		console.log("typeof clone.fullName = \"%s\"", typeof personByClassCloneByClone.fullName); // undefined
 	}
 }
