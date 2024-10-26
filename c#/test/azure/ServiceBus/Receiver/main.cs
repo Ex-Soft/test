31a4838da8406f4c927f8202d006f1deb396676f
Ex_Soft <4others2@gmail.com>
2024-10-26 14:32:18 +0300

    c# (azure servicebus)

--- c#/test/azure/ServiceBus/Receiver/main.cs
+++ c#/test/azure/ServiceBus/Receiver/main.cs
@@ -1,19 +1,45 @@
 ﻿using Azure.Messaging.ServiceBus;
 
-using static System.Console;
+ServiceBusClient client;
+ServiceBusProcessor processor;
+
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body} from subscription.");
+
+    // complete the message. messages is deleted from the subscription. 
+    //await args.CompleteMessageAsync(args.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
 
 const string connectionString = "Endpoint=sb://.servicebus.windows.net/;SharedAccessKeyName=;SharedAccessKey=;EntityPath=importdealeradmin";
 const string topicName = "importdealeradmin";
 const string subscriptionName = "importdealeradmin";
+client = new ServiceBusClient(connectionString);
+processor = client.CreateProcessor(topicName, subscriptionName, new ServiceBusProcessorOptions());
 
-ServiceBusClient client = new ServiceBusClient(connectionString);
-
-ServiceBusReceiverOptions receiverOptions = new ServiceBusReceiverOptions { SubQueue = SubQueue.DeadLetter };
-ServiceBusReceiver receiver = client.CreateReceiver(topicName, subscriptionName, receiverOptions);
-
-var message = await receiver.PeekMessageAsync();
-
-if (message?.Body != null)
+try
 {
-    WriteLine($"Body: {message.Body}");
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("Wait for a minute and then press any key to end the processing");
+    Console.ReadKey();
+
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
 }
