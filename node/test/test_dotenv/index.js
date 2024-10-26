762d7ef4bf6b3cd0425e426ac42c3951304a341f
Ex_Soft <4others2@gmail.com>
2024-10-26 17:18:32 +0300

    node (.env)

---
+++ node/test/test_dotenv/index.js
@@ -0,0 +1,11 @@
+import * as dotenv from "dotenv"
+
+dotenv.config();
+
+const env = process.env;
+
+console.log("TEST_ENV_VARIABLE1=%s", env.TEST_ENV_VARIABLE1);
+console.log("TEST_ENV_VARIABLE2=%s", env.TEST_ENV_VARIABLE2);
+console.log("TEST_ENV_VARIABLE3=%s", env.TEST_ENV_VARIABLE3);
+
+console.log("TEST_DEV_ENV_VARIABLE=%s", env.TEST_DEV_ENV_VARIABLE);
