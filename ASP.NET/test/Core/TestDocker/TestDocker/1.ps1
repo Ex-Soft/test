49e22f00890ac5fa1b8c626638cec69af4c4279b
Ex_Soft <4others2@gmail.com>
2024-10-29 07:01:51 +0200

    asp.net

--- ASP.NET/test/Core/TestDocker/TestDocker/1.ps1
+++ ASP.NET/test/Core/TestDocker/TestDocker/1.ps1
@@ -1,3 +1,9 @@
 docker-compose -f docker-compose.yml build
 docker compose up -d
 docker compose down --volumes
+
+docker ps -a
+docker exec -it -u root 44f6bf0370f4 /bin/bash
+
+apt-get update && apt-get -y install curl/stable-security
+curl -f http://localhost:54816/healthz
