f041e03d07c27daba9bbe6cb9c75a373a05993f1
Ex_Soft <4others2@gmail.com>
2024-11-12 17:52:10 +0200

    sql (m$ sql)

--- sql/MSSQL/Staff/StaffData.sql
+++ sql/MSSQL/Staff/StaffData.sql
@@ -1,8 +1,17 @@
-use testdb
+use testdb;
 go
 
-delete from Staff
+delete from Staff;
 go
+
+insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'George Washington', 1000, 1, N'17320222', null);
+insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'John Adams', 2000, 1, N'17351030', null);
+insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Thomas Jefferson', 3000, 1, N'17430413', null);
+
+insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Кравчук Леонид Макарович', 333, 380, N'19340110', null);
+insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Кучма Леонид Данилович', 444, 380, N'19380809', null);
+insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Ющенко Виктор Андреевич', 111, 380, N'19540223', null);
+insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Янукович Виктор Федорович', 555, 380, N'19500709', null);
 
 insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Ленин Владимир Ильич', 1000, 1, N'18700422', null)
 insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Сталин Иосиф Виссарионович', 300, 1, N'18781218', null)
@@ -11,14 +20,9 @@ insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Брежн
 insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Андропов Юрий Владимирович', 10000, 1, N'19140615', null)
 insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Черненко Константин Устинович', 100.01, 1, N'19110924', null)
 insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Горбачев Михаил Сергеевич', 222, 1, N'19310302', null)
-insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Кравчук Леонид Макарович', 333, 2, N'19340110', null)
-insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Кучма Леонид Данилович', 444, 2, N'19380809', null)
-insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Ющенко Виктор Андреевич', 111, 2, N'19540223', null)
-insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Янукович Виктор Федорович', 555, 2, N'19500709', null)
 insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Иванов Иван Иванович', 3000, 3, null, null)
 insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Петров Петр Петрович', 5000, 3, null, null)
 insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Сидоров Сидор Сидорович', 1500, 3, null, null)
 insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Васильев Василий Василиевич', 3000, 3, null, null)
 insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Дем''ян Бэдний', 800, 4, N'18830401', null)
-insert into Staff (Name, Salary, Dep, BirthDate, NullField) values (N'Вашингтон Джордж', 8000, 5, N'17320222', null)
 go
--- sql/MSSQL/TestTableTypes/TestProcedureWithTypeTableStaff.sql
+++ sql/MSSQL/TestTableTypes/TestProcedureWithTypeTableStaff.sql
@@ -1,18 +1,34 @@
-if object_id('TestProcedureWithTypeTableStaff', 'p') is not null
-  drop procedure TestProcedureWithTypeTableStaff
+if object_id('dbo.TestProcedureWithTypeTableStaff', 'p') is not null
+  drop procedure dbo.TestProcedureWithTypeTableStaff;
 go
 
-create procedure TestProcedureWithTypeTableStaff
-  @Staff TypeTableStaff readonly
+create procedure dbo.TestProcedureWithTypeTableStaff
+  @Staff dbo.TypeTableStaff readonly
 as
 begin
 
-  set nocount on
+  set nocount on;
 
   select
     s.*
   from
-    Staff s
-    join @Staff ps on (ps.id=s.id) 
-end
+    dbo.Staff s
+    join @Staff ps on (ps.id = s.id);
+end;
 go
+
+declare @t dbo.TypeTableStaff;
+insert into @t
+	(ID, [NAME])
+select
+	ID, [NAME]
+from
+(
+	values
+		(1, null),
+		(3, null)
+) t(ID, [NAME]);
+select * from @t;
+
+declare @return_value int;
+exec @return_value = dbo.TestProcedureWithTypeTableStaff @Staff = @t;
