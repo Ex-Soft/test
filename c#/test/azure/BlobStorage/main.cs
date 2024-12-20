04e012c57c643d2f70dc6907da1fe4f021f0b7e4
Ex_Soft <4others2@gmail.com>
2024-12-20 07:41:37 +0200

    c#

--- c#/test/azure/BlobStorage/main.cs
+++ c#/test/azure/BlobStorage/main.cs
@@ -3,13 +3,14 @@ using Azure.Storage.Blobs;
 
 const string accountName = "accountName";
 const string accountKey = "accountKey";
-StorageSharedKeyCredential credential = new StorageSharedKeyCredential(accountName, accountKey);
+StorageSharedKeyCredential? credential = !string.IsNullOrWhiteSpace(accountKey) ? new StorageSharedKeyCredential(accountName, accountKey) : null;
 const string blobContainerName = "blobContainerName";
+const string blobName = "blobName";
 
 try
 {
-    BlobServiceClient client = new(new Uri($"https://{accountName}.blob.core.windows.net"), credential);
-    BlobContainerClient blobContainerClient = client.GetBlobContainerClient(blobContainerName);
+    BlobServiceClient blobServiceClient = credential != null ? new BlobServiceClient(new Uri($"https://{accountName}.blob.core.windows.net"), credential) : new BlobServiceClient(new Uri($"https://{accountName}.blob.core.windows.net"));
+    BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
     var blobs = blobContainerClient.GetBlobs();
 
     foreach (var blob in blobs.Where(item => item.Properties.CreatedOn >= new DateTimeOffset(2023, 6, 1, 0, 0, 0, new TimeSpan(0))))
@@ -17,6 +18,9 @@ try
         System.Console.WriteLine($"{blob.Name}");
         System.Diagnostics.Debug.WriteLine($"{blob.Name}");
     }
+
+    BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+    blobClient.DownloadTo(blobName);
 }
 catch (Exception e)
 {
