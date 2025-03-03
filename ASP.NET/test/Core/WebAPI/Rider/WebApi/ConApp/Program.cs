c4a4d2f4c959c9bae1130ebf98dafd765a2525b7
Ex_Soft <4others2@gmail.com>
2025-03-03 07:21:51 +0200

    asp.net

---
+++ ASP.NET/test/Core/WebAPI/Rider/WebApi/ConApp/Program.cs
@@ -0,0 +1,11 @@
+﻿try
+{
+    using var client = new HttpClient();
+    client.BaseAddress = new Uri("http://localhost:5063");
+    client.DefaultRequestHeaders.Accept.Clear();
+    var response = await client.GetAsync("/weatherforecast");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
