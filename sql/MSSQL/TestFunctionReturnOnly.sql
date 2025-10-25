75be764393e583b038a748324c68c4556232b560
Ex_Soft <4others2@gmail.com>
2025-10-25 16:32:11 +0300

    sql

--- sql/MSSQL/TestFunctionReturnOnly.sql
+++ sql/MSSQL/TestFunctionReturnOnly.sql
@@ -1,8 +1,8 @@
-use testdb
+use testdb;
 go
 
 if object_id(N'TestFunctionReturnOnly', N'fn') is not null
-	drop function TestFunctionReturnOnly
+	drop function TestFunctionReturnOnly;
 go
 
 create function TestFunctionReturnOnly (@a int, @b int)
@@ -10,10 +10,10 @@ returns int
 as
 begin
 	declare
-		@result int
-    
-	set @result=@a+@b
-  
-	return @result
-end
+		@result int;
+
+	set @result = @a + @b;
+
+	return @result;
+end;
 go
--- sql/MSSQL/TestFunctionReturnTable.sql
+++ sql/MSSQL/TestFunctionReturnTable.sql
@@ -1,19 +1,18 @@
 /* http://stackoverflow.com/questions/586642/tsql-how-do-you-output-print-in-a-user-defined-function */
-use pretensions
+use testdb;
 go
 
-if object_id('TestFunctionReturnTable', 'TF') is not null
-  drop function TestFunctionReturnTable
+if object_id('TestFunctionReturnTable', 'tf') is not null
+  drop function TestFunctionReturnTable;
 go
 
 create function TestFunctionReturnTable()
 returns @t table(dt datetime)
 as
 begin
-  insert into @t values ('20101203')
-  return
-end
+  insert into @t values ('20101203');
+  return;
+end;
 go
 
-select * from TestFunctionReturnTable()
-go
+select * from TestFunctionReturnTable();
